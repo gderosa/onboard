7bc3507d9c442e4df0e36ededf933b01e8ee3144
gd <gd@localhost.(none)>
2009-11-11 19:20:36 +0000

    handle P-t-P interfaces with no peer

---
+++ modules/easy-rsa/etc/menu/crypto/easy-rsa.rb
@@ -0,0 +1,8 @@
+class OnBoard
+  MENU_ROOT.add_path('/crypto/easy-rsa', {
+    :href => '/crypto/easy-rsa',
+    :name => 'Key management',
+    :desc => 'Cryptographic certificates for OpenVPN and other applications',
+    :n    => 1
+  })
+end
--- modules/openvpn/etc/menu/network/openvpn/key-management.rb
+++
@@ -1,8 +0,0 @@
-class OnBoard
-  MENU_ROOT.add_path('/network/openvpn/key-management', {
-    :href => '/network/openvpn/key-management',
-    :name => 'Key management',
-    :desc => 'Create, sign, or revoke cryptographic certificates for VPN clients and servers',
-    :n    => 1
-  })
-end
