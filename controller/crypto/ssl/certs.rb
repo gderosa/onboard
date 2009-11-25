bbfe8eb18d971be69d9273b25627bc8d50d48088
Guido De Rosa <job@guidoderosa.net>
2009-11-25 19:00:54 +0000

    [m] SSL: still working on deleting certs.

--- controller/crypto/ssl/certs.rb
+++ controller/crypto/ssl/certs.rb
@@ -1,6 +1,14 @@
 class OnBoard
   class Controller  
+    # A WebService client does not need an entity-body (headers and Status
+    # will suffice), so html is fine as well, since it will be ignored...
     delete '/crypto/ssl/certs/:name.crt' do
+      format(
+        :path => '/crypto/ssl/cert_del',
+        :format => 'html',
+        :objects => {},
+        :msg => {:ok => true} 
+      )
     end
   end
 end
