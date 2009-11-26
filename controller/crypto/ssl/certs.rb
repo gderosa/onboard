4be698580ff599d85152130bf5a4b5f5a8aa2516
Guido De Rosa <job@guidoderosa.net>
2009-11-26 18:39:51 +0000

    SSL: certificate upload

--- controller/crypto/ssl/certs.rb
+++ controller/crypto/ssl/certs.rb
@@ -1,4 +1,5 @@
-require 'fileutils'
+autoload :FileUtils,  'fileutils'
+autoload :OpenSSL,    'onboard/extensions/openssl'
 
 class OnBoard
   class Controller  
@@ -31,9 +32,42 @@ class OnBoard
     end
 
     post '/crypto/ssl/certs.:format' do
-      msg = {}
+      target = nil
+      msg = {:ok => true} 
+      begin
+        cert = OpenSSL::X509::Certificate.new(
+            params['certificate'][:tempfile].read
+        )
+        cn = cert.to_h['subject']['CN']
+        target = "#{Crypto::SSL::CERTDIR}/#{cn}.crt"
+        if File.readable? target # already exists
+          begin # check if it's valid
+            OpenSSL::X509::Certificate.new(File.read target)
+            status(409)
+            msg = {
+              :ok => false, 
+              :err => "A certificate with the same COMMON NAME (#{cn}) and/or filename already exists!"
+            }
+          rescue OpenSSL::X509::CertificateError # otherwise you can overwrite
+            File.open(target, 'w') do |f|
+              # the same format created by easy-rsa...
+              f.write cert.to_text # human readable data
+              f.write cert.to_s # the certificate itself between BEGIN-END tags
+            end           
+          end
+        else
+          File.open(target, 'w') do |f|
+            # the same format created by easy-rsa...
+            f.write cert.to_text # human readable data
+            f.write cert.to_s # the certificate itself between BEGIN-END tags
+          end
+        end
+      rescue OpenSSL::X509::CertificateError
+        status(400)
+        msg = {:ok => false, :err => $!}
+      end
       format(
-        :path     => '/crypto/ssl/cert-create',
+        :path     => '/crypto/ssl/cert_create',
         :format   => params[:format],
         :objects  => nil,
         :msg      => msg
