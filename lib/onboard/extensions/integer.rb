776feafadced395e8a463b57c58ab8cd32b9943e
Guido De Rosa <job@guidoderosa.net>
2009-11-27 18:09:13 +0000

    EasyRSA: view serial numbers

---
+++ lib/onboard/extensions/integer.rb
@@ -0,0 +1,10 @@
+class Integer
+  # returns a String of hexadecimal bytes, e.g. 02:13:af
+  def to_byte_s(byte_separator=':')
+    s = to_s(16)
+    if s.length.odd?
+      s = '0' + s 
+    end
+    s.scan(/../).join(byte_separator) 
+  end
+end
