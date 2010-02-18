f4373edb1176c41b1d15da01c101f473cfe40c5f
Guido De Rosa <job@guidoderosa.net>
2010-02-18 12:09:14 +0000

    Chilli: view configuration details

--- modules/chilli/controller/network/access-control/chilli.rb
+++ modules/chilli/controller/network/access-control/chilli.rb
@@ -15,7 +15,17 @@ class OnBoard
         :objects  => all
       )
     end
-    
+
+    get '/network/access-control/chilli/:ifname.:format' do
+      all = OnBoard::Network::AccessControl::Chilli.getAll()
+      format(
+        :module => 'chilli',
+        :path => '/network/access-control/chilli/ifconfig',
+        :format => params[:format],
+        :objects  => all.detect{|x| x.conf['dhcpif'] == params[:ifname]} 
+      )
+    end
+   
   end
 
 end
