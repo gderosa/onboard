e8ca5bfdee583193008dbd61769b5e345bcc4167
Ruby OnBoard <onboard@debvrouter.localdomain>
2010-03-09 15:58:00 +0100

    hotspotlogin: load.rb (ouch!)

---
+++ modules/hotspotlogin/load.rb
@@ -0,0 +1,13 @@
+class OnBoard
+  module Service
+    class HotSpotLogin
+      ROOTDIR = File.dirname(__FILE__)
+      CONFDIR = File.join ROOTDIR, '/etc/config'
+      $LOAD_PATH.unshift  ROOTDIR + '/lib'
+      OnBoard.find_n_load ROOTDIR + '/etc/menu'
+      OnBoard.find_n_load ROOTDIR + '/controller'
+    end
+  end
+end
+
+
