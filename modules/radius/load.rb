6d7f8304a121d7835c9f1c6076f69f11585b3a0c
Guido De Rosa <job@guidoderosa.net>
2010-03-12 16:10:36 +0000

    radius: menu entry

---
+++ modules/radius/load.rb
@@ -0,0 +1,15 @@
+require 'fileutils'
+
+class OnBoard
+  module Service
+    module RADIUS
+      ROOTDIR = File.dirname(__FILE__)
+      #CONFDIR = File.join ROOTDIR, '/etc/config'
+      #$LOAD_PATH.unshift  ROOTDIR + '/lib'
+      OnBoard.find_n_load ROOTDIR + '/etc/menu'
+      #OnBoard.find_n_load ROOTDIR + '/controller'
+    end
+  end
+end
+
+
