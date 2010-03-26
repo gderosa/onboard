687d4e26840912ebbf655b4042d0f3191fd0742d
Guido De Rosa <job@guidoderosa.net>
2010-03-26 10:27:57 +0000

    openvpn: when creating new interface, avoid conflict also w/ non-running vpn

--- modules/openvpn/lib/onboard/network/openvpn/interface/name.rb
+++ modules/openvpn/lib/onboard/network/openvpn/interface/name.rb
@@ -7,7 +7,9 @@ class OnBoard
         module Name
           def self.generate
             n = 0
-            names = Interface.getAll.map{|x| x.name}
+            names = 
+                Interface.getAll.map  {|x| x.name}        |
+                VPN.all_cached.map    {|x| x.data['dev']} 
             name_ = nil
             while names.include? (name_ = "ovpn#{n}")
               n += 1
