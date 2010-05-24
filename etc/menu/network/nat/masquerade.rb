eebabe71135e73c4393be9449e17490aa5ec745b
Guido De Rosa <guido.derosa@vemarsas.it>
2010-05-24 19:40:20 +0200

    toward integrating SNAT and MASQUERADE - and detect the "default route interface" to give a default choice for outgoing netif

--- etc/menu/network/nat/masquerade.rb
+++
@@ -1,8 +0,0 @@
-class OnBoard
-  MENU_ROOT.add_path('/network/nat/masquerade', {
-    :href => '/network/nat/masquerade',
-    :name => 'Internet Connection Sharing',
-    :desc => 'Also known as &ldquo;IP Masquerading&rdquo;; connect several machines with a single public address',
-    :n    => -1
-  })
-end
