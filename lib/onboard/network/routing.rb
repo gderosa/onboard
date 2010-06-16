c74c8803500c2c6aafe3449a925de18adf220d9f
Guido De Rosa <guido.derosa@vemarsas.it>
2010-06-16 09:54:54 +0000

    re-add lib/onboard/network/routing.rb

---
+++ lib/onboard/network/routing.rb
@@ -0,0 +1,11 @@
+require 'onboard/network/routing/constants'
+
+class OnBoard
+  module Network
+    module Routing
+      autoload :Table,  'onboarb/network/routing/table'
+      autoload :Route,  'onboarb/network/routing/route'
+      autoload :Rule,   'onboarb/network/routing/rule'
+    end
+  end
+end
