3995280524fe1314e9a8df8291d2b6a953dd1dda
Guido De Rosa <job@guidoderosa.net>
2010-07-08 20:32:48 +0200

    wait relevant threads (on restore...)

--- lib/onboard/system/command.rb
+++ lib/onboard/system/command.rb
@@ -43,6 +43,8 @@ class OnBoard
           stdout.close
           stderr.close 
         end
+        wait_thr[:onboard_wait_me] = true
+        wait_thr[:description] = cmd
         return msg
       end
 
