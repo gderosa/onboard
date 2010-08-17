6ed2476c0bc818147111280c454588b6681a4061
Guido De Rosa <guido.derosa@vemarsas.it>
2010-08-17 10:26:06 +0000

    updated shell scripts

---
+++ common.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ ! -d $ONBOARD_DATADIR ]; then
+	ONBOARD_DATADIR=/home/onboard/.onboard
+fi
+
+VARRUN="$ONBOARD_DATADIR/var/run"
+
+if [ ! -d $VARRUN ]; then
+	mkdir -p $VARRUN
+fi
+
+ENV_SH=$ONBOARD_DATADIR/etc/config/env.sh
+
+LANG='en_US.UTF-8' 
+ONBOARD_ENVIRONMENT='production'
+
+if [ -r $ENV_SH ]; then
+	. $ENV_SH 
+fi
+
+export ONBOARD_ENVIRONMENT
+
