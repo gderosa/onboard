6d5e97a49b7a1c06ad190ff9114984ab99be0557
Guido De Rosa <guido.derosa@vemarsas.it>
2010-08-18 14:25:04 +0000

    Config system migration script: some fixes
    * Use rsync to take advantage of --exclude

--- etc/scripts/migrate-config.sh
+++ etc/scripts/migrate-config.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-ONBOARD_HOME=/home/onboard
+# You may set ONBOARD_ROOTDIR and ONBOARD_DATADIR in the environment
+# to existent directories in order to customize this script's behavior;
+# ONBOARD_HOME will be unused in such case.
+
+ONBOARD_HOME=/home/onboard 
 
 if [ ! -d "$ONBOARD_ROOTDIR" ] ; then
 	ONBOARD_ROOTDIR=$ONBOARD_HOME/onboard
@@ -10,12 +14,17 @@ if [ ! -d "$ONBOARD_DATADIR" ] ; then
 fi
 
 mkdir -p $ONBOARD_DATADIR/etc/config
-cp -rvf $ONBOARD_ROOTDIR/etc/config $ONBOARD_DATADIR/etc/
-cp -rvf $ONBOARD_ROOTDIR/modules/openvpn/etc/config $ONBOARD_DATADIR/etc/config
 
-mkdir -p "$ONBOARD_DATADIR/var/lib/var/lib/crypto/easy-rsa/keys"
-cp -rvf \
-	$ONBOARD_ROOTDIR/modules/easy-rsa/easy-rsa/2.0/keys 		\
-	$ONBOARD_DATADIR/var/lib/var/lib/crypto/easy-rsa/		\
+rsync \
+	--exclude config/network/dnsmasq/defaults \
+	-a $ONBOARD_ROOTDIR/etc/config $ONBOARD_DATADIR/etc/ \
+
+rsync -a $ONBOARD_ROOTDIR/modules/openvpn/etc/config $ONBOARD_DATADIR/etc/
+
+
+mkdir -p "$ONBOARD_DATADIR/var/lib/crypto/easy-rsa/keys"
+rsync -a \
+	$ONBOARD_ROOTDIR/modules/easy-rsa/easy-rsa/2.0/keys 	\
+	$ONBOARD_DATADIR/var/lib/crypto/easy-rsa/		\
 
 
