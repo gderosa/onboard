a7f05e3273feed545aaf941778bdc5071186c701
Guido De Rosa <job@guidoderosa.net>
2010-09-23 16:53:11 +0200

    Menu code moved to an external library.
    You will find hierarchical_menu on github.com and rubygems.org .

--- lib/onboard/menu/node.rb
+++ lib/onboard/menu/node.rb
@@ -1,52 +1,10 @@
-require 'tree'
-
-require 'onboard/extensions/tree'
+require 'hmenu'
 
 class OnBoard
   module Menu
-    class MenuNode < Tree::TreeNode
-      def to_html_ul
-        # TODO: use CSS+Javascript to show/hide subitems on click
-        s = ""
-        if isRoot?
-          s << "<a href=\"/\">Home</a>"
-        else
-          if content 
-            if content[:href]
-              s << 
-                "<a " << 
-                  "title=\"#{content[:desc]}\" " << 
-                  "href=\"#{content[:href]}.html\">#{content[:name]}" <<
-                "</a>"
-            elsif content[:name]
-              s << 
-                  '<span title="' << (content[:desc] || '') << '">' << 
-                    (content[:name] || '') << 
-                  '</span>'
-            end
-          else
-            s << name.capitalize
-          end
-        end
-        if hasChildren?
-          s += "<ul>"
-          children.sort.each do |child|
-            s += "<li>" << child.to_html_ul << "</li>"
-          end
-          s += "</ul>"
-        end
-        return s
-      end
-      def <=>(other) # for sorting
-        n <=> other.n 
-      end
-      def n
-        begin
-          content[:n] ? content[:n] : 0
-        rescue
-          0
-        end
-      end
-    end
+    # very simple "delegation" to +HMenu::Node+ class. See documentation
+    # for 'hierarchical_menu' on rubygems.org .
+    class MenuNode < ::HMenu::Node; end
   end
 end
+
