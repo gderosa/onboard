94be9cdd704c9617ed1ea8200c2f082f6b3e9de5
Guido De Rosa <guido.derosa@vemarsas.it>
2010-10-05 01:27:19 +0000

    openvpn/menu: better children handling (even on Not Found)

--- modules/openvpn/etc/menu/network/openvpn.rb
+++ modules/openvpn/etc/menu/network/openvpn.rb
@@ -1,7 +1,7 @@
 class OnBoard
   MENU_ROOT.add_path('/network/openvpn', {
     :href => '/network/openvpn',
-    :children => %r{^/network/openvpn/vpn/.+},
+    :children => %r{^/network/openvpn/.*},
     :name => 'OpenVPN',
     :desc => 'Virtual Private Networks',
     :n    => 2
