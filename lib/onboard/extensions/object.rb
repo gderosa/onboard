33c1207e4ff62ef84db55d41609c1d1d93ca888a
Guido De Rosa <guido.derosa@vemarsas.it>
2010-10-08 19:20:27 +0000

    core extensions: deep structure recursive methods

--- lib/onboard/extensions/object.rb
+++ lib/onboard/extensions/object.rb
@@ -24,24 +24,6 @@ class Object
     end
   end
 
-  def deep_map(&block)
-    if self.respond_to? :each_pair
-      out = {}
-      self.each_pair do |k, v|
-        out[k] = v.deep_map(&block)
-      end
-      return out
-    elsif self.respond_to? :each
-      out = []
-      self.each do |x|
-        out << x.deep_map(&block)
-      end
-      return out
-    else
-      return block.call(self)
-    end
-  end
-
 end
 
 
