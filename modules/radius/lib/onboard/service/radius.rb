a038a6ff18a6c30ed2c5bb6349fc9fb503e79b24
Guido De Rosa <guido.derosa@vemarsas.it>
2010-11-18 15:37:47 +0000

    Revert "remove radius module"
    This reverts commit 5b49602f9dca2889607c5c1c59ab6d9bc5e125b2.

---
+++ modules/radius/lib/onboard/service/radius.rb
@@ -0,0 +1,48 @@
+require 'sequel'
+
+class OnBoard
+  module Service
+    module RADIUS
+      
+      autoload :Accounting, 'onboard/service/radius/accounting'
+
+      CONFFILE = File.join CONFDIR, 'current/radius.conf.yaml'
+
+      def self.read_conf
+        if File.readable? CONFFILE
+          YAML.load(File.read CONFFILE)
+        else
+          {}
+        end
+      end
+
+      def self.write_conf(h)
+        File.open CONFFILE, 'w' do |f|
+          f.write h.to_yaml
+        end
+      end
+
+      def self.db
+        @@db = db_connect unless 
+            class_variable_defined? :@@db
+        return @@db
+      end
+
+      def self.db_connect
+        conf = read_conf
+        @@db = Sequel.connect( "mysql://#{conf['dbhost']}/#{conf['dbname']}",
+          :user     => conf['dbuser'],
+          :password => conf['dbpass']
+        ) 
+      end
+
+      def self.db_disconnect; @@db.disconnect; end
+
+      def self.db_reconnect
+        db_disconnect
+        db_connect
+      end
+
+    end
+  end
+end
