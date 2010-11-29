b56e0f0232c065ff7614c89f13bf35c5ec386164
Guido De Rosa <job@guidoderosa.net>
2010-11-29 22:30:12 +0100

    pagination: remove link from current page

--- lib/onboard/pagination.rb
+++ lib/onboard/pagination.rb
@@ -1,8 +1,28 @@
 class OnBoard
   module Pagination
+
     DEFAULTS = {
       :page     =>  1,
-      :per_page => 25
+      :per_page => 10,
     }
+
+    class << self
+
+      def normalize_params(params)
+        h = {}
+        DEFAULTS.each_pair do |k, v|
+          unless params[k] and params[k].to_i > 0
+            h[k] = v
+          end
+        end
+        params.update h
+      end
+
+      def normalize_params!(params)
+        params = normalize_params(params)
+      end
+
+    end
+
   end
 end
