f7e3590a0fb97d1e142a8a878cf871da686729ab
Guido De Rosa <job@guidoderosa.net>
2010-12-01 15:08:13 +0100

    stub digest/md5 extension

---
+++ lib/onboard/extensions/digest/md5.rb
@@ -0,0 +1,20 @@
+module Digest
+  class MD5
+    
+    LENGTH = 256
+
+    alias digest_orig digest
+
+    def digest(str, opts_h=nil)
+      if opts_h
+        if opts_h[:salt]
+          salt = opts_h[:salt]
+          return digest_orig(str + salt) + salt
+        end
+      else
+        return digest_orig digest str
+      end
+    end
+
+  end
+end
