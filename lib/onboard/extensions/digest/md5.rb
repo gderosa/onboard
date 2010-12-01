67565a69c5b607ab7faee3d4ba3b969a1330d437
Guido De Rosa <job@guidoderosa.net>
2010-12-01 15:17:12 +0100

    digest/md5 extension: class << self

--- lib/onboard/extensions/digest/md5.rb
+++ lib/onboard/extensions/digest/md5.rb
@@ -5,17 +5,21 @@ module Digest
     
     LENGTH = 256
 
-    alias digest_orig digest
+    class << self
 
-    def digest(str, opts_h=nil)
-      if opts_h
-        if opts_h[:salt]
-          salt = opts_h[:salt]
-          return digest_orig(str + salt) + salt
+      alias digest_orig digest
+
+      def digest(str, opts_h=nil)
+        if opts_h
+          if opts_h[:salt]
+            salt = opts_h[:salt]
+            return digest_orig(str + salt) + salt
+          end
+        else
+          return digest_orig str
         end
-      else
-        return digest_orig digest str
       end
+
     end
 
   end
