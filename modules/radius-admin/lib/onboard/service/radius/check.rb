90fe04b9dd5be558c9309d60cbc786ee1f7448cf
Guido De Rosa <guido.derosa@vemarsas.it>
2010-12-17 10:07:03 +0000

    radius-admin: adopt Check::setup

--- modules/radius-admin/lib/onboard/service/radius/check.rb
+++ modules/radius-admin/lib/onboard/service/radius/check.rb
@@ -12,53 +12,59 @@ class OnBoard
 
         class << self
 
+          def setup
+            @@conf    ||= RADIUS.read_conf
+            @@table   ||= @@conf['user']['check']['table'].to_sym
+            @@columns ||= @@conf['user']['check']['columns'].symbolize_values
+          end
+
+          def setup!
+            @@conf = @@table = @@columns = nil
+          end
+
           def get(params)
-            conf      = RADIUS.read_conf
-            table     = conf['user']['check']['table'].to_sym
-            columns   = conf['user']['check']['columns']
+            setup
             page      = params[:page].to_i
             per_page  = params[:per_page].to_i
-            select    = RADIUS.db[table].select(
-              *columns.symbolize_all.values
-            ).order_by columns['User-Name'].to_sym
+            select    = RADIUS.db[@@table].select(
+              *@@columns.values
+            ).order_by @@columns['User-Name']
             {
-              'columns'     => columns,
+              #'columns'     => columns,
               'rows'        => select.paginate(page, per_page).to_a,
               'total_items' => select.count,
               'page'        => page,
               'per_page'    => per_page
             }
           end
-
+  
         
           def insert(params)
-            conf      = RADIUS.read_conf
-            table     = conf['user']['check']['table'].to_sym
-            col       = conf['user']['check']['columns'].symbolize_values
+            setup
             if params['check']['User-Password'] != 
                 params['confirm']['check']['User-Password']
               raise PasswordsDoNotMatch, 'Passwords do not match!'
             end
-            if RADIUS.db[table].where(
-                col['User-Name'] => params['check']['User-Name'] ).any?
-              raise UserAlreadyExists, "User '#{Rack::Utils::escape_html params['check']['User-Name']}' already exists!"
+            if RADIUS.db[@@table].where(
+                @@columns['User-Name'] => params['check']['User-Name'] ).any?
+              raise UserAlreadyExists, "User '#{params['check']['User-Name']}' already exists!"
             end
             if params['check']['Password-Type'] =~ /\S/
-              RADIUS.db[table].insert(
-                col['User-Name']  => params['check']['User-Name'],
-                col['Operator']   => ':=',
-                col['Attribute']  => params['check']['Password-Type'],
-                col['Value']      => RADIUS.compute_password(
+              RADIUS.db[@@table].insert(
+                @@columns['User-Name']  => params['check']['User-Name'],
+                @@columns['Operator']   => ':=',
+                @@columns['Attribute']  => params['check']['Password-Type'],
+                @@columns['Value']      => RADIUS.compute_password(
                   :type             => params['check']['Password-Type'],
                   :cleartext        => params['check']['User-Password']
                 ),
               )
             end
-            RADIUS.db[table].insert(
-              col['User-Name']  => params['check']['User-Name'],
-              col['Operator']   => ':=',
-              col['Attribute']  => 'Auth-Type',
-              col['Value']      => params['check']['Auth-Type'],
+            RADIUS.db[@@table].insert(
+              @@columns['User-Name']  => params['check']['User-Name'],
+              @@columns['Operator']   => ':=',
+              @@columns['Attribute']  => 'Auth-Type',
+              @@columns['Value']      => params['check']['Auth-Type'],
             ) if params['check']['Auth-Type'] =~ /\S/
           end
 
