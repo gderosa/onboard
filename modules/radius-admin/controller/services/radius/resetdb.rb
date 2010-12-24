902f85c41e72859c688c93e9332547a354a70d96
Guido De Rosa <guido.derosa@vemarsas.it>
2010-12-24 11:36:37 +0000

    radius-admin: [stub] db reset

--- modules/radius-admin/controller/services/radius/resetdb.rb
+++ modules/radius-admin/controller/services/radius/resetdb.rb
@@ -2,6 +2,7 @@ require 'facets/hash'
 require 'sinatra/base'
 
 require 'onboard/service/radius'
+require 'onboard/service/radius/db'
 
 class OnBoard
   class Controller < Sinatra::Base
@@ -13,6 +14,32 @@ class OnBoard
         :title    => 'RADIUS Database reset',
         :format   => 'html',
         :formats  => %w{html}
+      )
+    end
+
+    get '/services/radius/dbdata.:format' do
+      redirect "/services/radius/dbdata.params[:format]" 
+    end
+
+    delete '/services/radius/dbdata.:format' do
+      deleted = []
+      msg     = handle_errors do
+        deleted = Service::RADIUS::Db.reset_data(params)
+      end
+      unless msg[:err]
+        if deleted.length > 0
+          msg[:info] = "Data has been deleted!"
+        else
+          msg[:info] = "RADIUS data have been kept"
+        end
+      end
+      format(
+        :module   => 'radius-admin',
+        :path     => '/services/radius/dbdata',
+        :title    => 'RADIUS Database reset',
+        :format   => params[:format],
+        :objects  => nil,
+        :msg      => msg
       )
     end
 
---
+++ modules/radius-admin/lib/onboard/service/radius/db.rb
@@ -0,0 +1,14 @@
+class OnBoard
+  module Service
+    module RADIUS
+      module Db
+        class << self
+          def reset_data(params)
+            #noop
+            return []
+          end
+        end
+      end
+    end
+  end
+end
