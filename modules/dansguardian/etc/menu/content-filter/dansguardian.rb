7fb4162e6403c9329ca8bc89e2fd0e2538c7bc32
Guido De Rosa <guido.derosa@vemarsas.it>
2011-02-09 09:00:27 +0000

    dansguardian: menu: black/white lists

--- modules/dansguardian/etc/menu/content-filter/dansguardian.rb
+++ modules/dansguardian/etc/menu/content-filter/dansguardian.rb
@@ -3,6 +3,7 @@ class OnBoard
     :name => 'Content Filtering',
     :n    => 2
   })
+
   MENU_ROOT.add_path('/content-filter/dg', {
     :href => '/content-filter/dansguardian',
     :name => 'DansGuardian',
@@ -13,6 +14,75 @@ class OnBoard
     :name => 'Filter Groups',
     :desc => 'You may configure different filtering rules for each group of users',
   })
+
+  
+  MENU_ROOT.add_path('/content-filter/dg/lists', {
+    :name => 'Lists',
+    :n    => 2
+  })
+
+  MENU_ROOT.add_path('/content-filter/dg/lists/banned', {
+    :name => 'Black lists',
+    :n    => 1
+  })
+
+  MENU_ROOT.add_path('/content-filter/dg/lists/banned/sites', {
+    :href => '/content-filter/dansguardian/lists/banned/sites',
+    :name => 'Sites',
+    :n    => 1
+  })
+  MENU_ROOT.add_path('/content-filter/dg/lists/banned/URLs', {
+    :href => '/content-filter/dansguardian/lists/banned/URLs',
+    :name => 'URLs',
+    :n    => 2
+  })
+  MENU_ROOT.add_path('/content-filter/dg/lists/banned/phrases', {
+    :href => '/content-filter/dansguardian/lists/banned/phrases',
+    :name => 'Phrases',
+    :n    => 3
+  })
+  MENU_ROOT.add_path('/content-filter/dg/lists/banned/extensions', {
+    :href => '/content-filter/dansguardian/lists/banned/extensions',
+    :name => 'File extensions',
+    :n    => 4
+  })
+  MENU_ROOT.add_path('/content-filter/dg/lists/banned/MIMEtypes', {
+    :href => '/content-filter/dansguardian/lists/banned/MIMEtypes',
+    :name => 'MIME types',
+    :n    => 5
+  })
+
+  MENU_ROOT.add_path('/content-filter/dg/lists/exception', {
+    :name => 'White lists',
+    :n    => 2
+  })
+  MENU_ROOT.add_path('/content-filter/dg/lists/exception/sites', {
+    :href => '/content-filter/dansguardian/lists/exception/sites',
+    :name => 'Sites',
+    :n    => 1
+  })
+  MENU_ROOT.add_path('/content-filter/dg/lists/exception/URLs', {
+    :href => '/content-filter/dansguardian/lists/exception/URLs',
+    :name => 'URLs',
+    :n    => 2
+  })
+  MENU_ROOT.add_path('/content-filter/dg/lists/exception/phrases', {
+    :href => '/content-filter/dansguardian/lists/exception/phrases',
+    :name => 'Phrases',
+    :n    => 3
+  })
+  MENU_ROOT.add_path('/content-filter/dg/lists/exception/extensions', {
+    :href => '/content-filter/dansguardian/lists/exception/extensions',
+    :name => 'File extensions',
+    :n    => 4
+  })
+  MENU_ROOT.add_path('/content-filter/dg/lists/exception/MIMEtypes', {
+    :href => '/content-filter/dansguardian/lists/exception/MIMEtypes',
+    :name => 'MIME types',
+    :n    => 5
+  })
+
+
 
 end
 
