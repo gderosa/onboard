47425bef785514e403d2613f574ccdef11fe5343
Guido De Rosa <guido.derosa@vemarsas.it>
2011-02-09 18:11:53 +0000

    dansguardian: lists: GET controller stub

---
+++ modules/dansguardian/controller/content-filter/dansguardian/lists.rb
@@ -0,0 +1,30 @@
+require 'sinatra/base'
+require 'onboard/content-filter/dg'
+
+class OnBoard
+  class Controller < Sinatra::Base
+
+    %w{
+      /content-filter/dansguardian/lists/*/*/*.:format
+      /content-filter/dansguardian/lists/*/*.:format
+    }.each do |path| # Order matters.
+      #
+      # Example: /content-filter/dansguardian/lists/banned/sites/sub1/sub2.html
+      # You get params[:splat] #=> ["banned", "sites", "sub1/sub2" ]
+      #
+      # Example: /content-filter/dansguardian/lists/banned/sites.html
+      # You get params[:splat] #=> ["banned", "sites"]
+      get path do
+        format(
+          :path     => '/content-filter/dansguardian/lists',
+          :module   => 'dansguardian',
+          :title    => 'DansGuardian: Lists',
+          :format   => params[:format],
+          :objects  => params[:splat] 
+        )
+      end
+
+    end
+   
+  end
+end
