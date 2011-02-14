82a2733f16359333d2fe2727cbd041a8739dbda0
Guido De Rosa <guido.derosa@vemarsas.it>
2011-02-14 18:34:41 +0000

    dansguardian: ManagedList::List#includables

--- modules/dansguardian/lib/onboard/content-filter/dg/managed-list/list.rb
+++ modules/dansguardian/lib/onboard/content-filter/dg/managed-list/list.rb
@@ -1,3 +1,4 @@
+require 'find'
 require 'dansguardian/list'
 require 'onboard/content-filter/dg/managed-list/filepath-mixin'
 
@@ -20,11 +21,30 @@ class OnBoard
           def listcategory; @data.listcategory; end
           def read!;        @data.read!;        end
 
-          def includes
-            @data.includes.map do |abspath| 
-              relpath = ManagedList.relative_path abspath
-              ManagedList::List.new :relative_path => relpath
+          #def includes
+          #  @data.includes.map do |abspath| 
+          #    relpath = ManagedList.relative_path abspath
+          #    ManagedList::List.new :relative_path => relpath
+          #  end
+          #end 
+
+          def includables
+            includes    = @data.includes
+            dir_name    = File.dirname absolute_path
+            includables = []
+            ::Dir.glob "#{dir_name}/*/**" do |f|
+              unless File.directory? File.realpath f
+                relpath   = ManagedList.relative_path f
+                list      = ManagedList::List.new :relative_path => relpath
+                if @data.includes.include? f
+                  def list.included?; true;   end
+                else
+                  def list.included?; false;  end
+                end
+                includables << list
+              end
             end
+            return includables
           end
 
           def <=>(other)
