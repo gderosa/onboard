4a5e2c592f08adef5ea54d85acd6e89fd6d36368
Guido De Rosa <job@guidoderosa.net>
2011-02-20 22:53:50 +0100

    dansguardian: edit filtergroup!

--- modules/dansguardian/lib/onboard/content-filter/dg/filter-group.rb
+++ modules/dansguardian/lib/onboard/content-filter/dg/filter-group.rb
@@ -1,3 +1,5 @@
+require 'dansguardian'
+
 class OnBoard
   module ContentFilter
     class DG
@@ -18,6 +20,40 @@ class OnBoard
 
         def update!(params)
           pp params
+          
+          # Update Hash: will be the argument for ::DansGuardian::Updater.update!
+          u = {} 
+          u['groupname']        = params['groupname']
+          u['naughtynesslimit'] = params['naughtynesslimit']
+          u['groupmode'] =  
+              ::DansGuardian::Config::FilterGroup::GROUPMODE.invert[ 
+                  params['groupmode'].to_sym 
+              ]
+          u['disablecontentscan'] = case params['enablecontentscan']
+                                    when 'on'
+                                      'off'
+                                    else
+                                      'on'
+                                    end
+          u['blockdownloads'] = case params['blockdownloads']
+                                    when 'on'
+                                      'on'
+                                    else
+                                      'off'
+                                    end
+          u['weightedphrasemode'] = case params['weightedphrasemode']
+                                    when ''
+                                      :remove!
+                                    else
+                                      params['weightedphrasemode']
+                                    end
+          params.each_pair do |key, value|
+            if key =~ /list$/
+              u[key] = DG::ManagedList.absolute_path value
+            end
+          end
+
+          ::DansGuardian::Updater.update!(@file, u)
         end
 
       end
