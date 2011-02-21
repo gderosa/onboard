d328feac02691e479fa4067f12c4326c14ba59ef
Guido De Rosa <guido.derosa@vemarsas.it>
2011-02-21 18:15:43 +0000

    dansguardian: move main config in a submenu

--- modules/dansguardian/etc/menu/content-filter/dansguardian.rb
+++ modules/dansguardian/etc/menu/content-filter/dansguardian.rb
@@ -5,10 +5,16 @@ class OnBoard
   })
 
   MENU_ROOT.add_path('/content-filter/dg', {
-    :href => '/content-filter/dansguardian',
+    #:href => '/content-filter/dansguardian',
     :name => 'DansGuardian',
     :desc => 'DansGuardian Web Content Filtering',
   })
+  MENU_ROOT.add_path('/content-filter/dg/main', {
+    :href => '/content-filter/dansguardian',
+    :name => 'Main',
+    :n    => -10
+  })
+ 
   MENU_ROOT.add_path('/content-filter/dg/fg', {
     :href => '/content-filter/dansguardian/filtergroups',
     :name => 'Filter Groups',
