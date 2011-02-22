07d04b5f67a0009c5db6669dd57f0d94bd474567
Guido De Rosa <guido.derosa@vemarsas.it>
2011-02-22 17:59:25 +0000

    dansguardian: SQLAuth: rearrange pages

--- modules/dansguardian/controller/content-filter/dansguardian/authplugins/sql.rb
+++ modules/dansguardian/controller/content-filter/dansguardian/authplugins/sql.rb
@@ -4,17 +4,14 @@ require 'onboard/content-filter/dg'
 class OnBoard
   class Controller < Sinatra::Base
 
-    get '/content-filter/dansguardian/authplugins/sql.:format' do
-      #dgconf = ::DansGuardian::Config.new(
-      #  :mainfile => ::OnBoard::ContentFilter::DG.config_file
-      #)
+    get '/content-filter/dansguardian/authplugins/sql/db.:format' do
       sqlauth = ::DansGuardian::Config::Auth::SQL.new
       data    = ::DansGuardian::Parser.read_file (
         ::OnBoard::ContentFilter::DG::AuthPlugin.config_file(:sql) 
       )
       sqlauth.load data
       format(
-        :path     => '/content-filter/dansguardian/authplugins/sql',
+        :path     => '/content-filter/dansguardian/authplugins/sql/db',
         :module   => 'dansguardian',
         :title    => "DansGuardian: SQL/RADIUS Authentication",
         :format   => params[:format],
--- modules/dansguardian/etc/menu/content-filter/dansguardian/authplugins.rb
+++ modules/dansguardian/etc/menu/content-filter/dansguardian/authplugins.rb
@@ -3,7 +3,17 @@ class OnBoard
   MENU_ROOT.add_path('/content-filter/dg/authplugins/sql', {
     :name => 'SQL/RADIUS',
     :desc => 'Captive Portal and 802.1X integration',
-    :href => '/content-filter/dansguardian/authplugins/sql'
+  })
+
+  MENU_ROOT.add_path('/content-filter/dg/authplugins/sql/groups', {
+    :name => 'Group mapping',
+    :href => '/content-filter/dansguardian/authplugins/sql/groups',
+    :n    => 1
+  })
+  MENU_ROOT.add_path('/content-filter/dg/authplugins/sql/db', {
+    :name => 'Database and queries',
+    :href => '/content-filter/dansguardian/authplugins/sql/db',
+    :n    => 2
   })
 
 end
