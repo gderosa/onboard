f69c3f767e6ec0b0bb886eef4078440e2e6ddeae
Guido De Rosa <job@guidoderosa.net>
2011-03-10 19:29:35 +0100

    version bump 2011.03.01

--- onboard.rb
+++ onboard.rb
@@ -24,7 +24,7 @@ end
 
 class OnBoard
   LONGNAME          ||= 'OnBoard'
-  VERSION           = '2011.03.00.alpha2'
+  VERSION           = '2011.03.01'
 
   PLATFORM          = Platform::Debian # TODO? make it configurable? get rid of Platform?
 
