92c9f12b08e8d51d7c486229de9094e7c596adfa
Guido De Rosa <job@guidoderosa.net>
2011-05-27 13:11:29 +0200

    custom.css

--- controller/css.rb
+++ controller/css.rb
@@ -2,9 +2,13 @@ class OnBoard
   class Controller < Sinatra::Base
 
     # just to have "CSS variables"
-    get '/css/:stylesheet' do
+    get '/css/default.css' do
       content_type 'text/css'
-      erubis ('css/' + params[:stylesheet]).to_sym
+      erubis (:"css/default.css")
+    end
+
+    get '/css/custom.css' do
+      send_file CONFDIR + '/self/custom.css'
     end
 
   end
