3e138b24a31c44d7e8796b06317e2b2aa719e123
Guido De Rosa <guido.derosa@vemarsas.it>
2011-08-09 11:22:16 +0000

    radius selfcare r/w in signup.yml config file

--- modules/radius-admin/lib/onboard/service/radius/signup.rb
+++ modules/radius-admin/lib/onboard/service/radius/signup.rb
@@ -9,12 +9,13 @@ class OnBoard
         CONFFILE = File.join RADIUS::CONFDIR, 'signup.yml'
 
         DEFAULT_CONFIG = {
-          'enable'  => false,
-          'check'   => {
-            'Password-Type' => 'SSHA1-Password',
-            'Auth-Type'     => ''
+          'enable'          => false,
+          'enable_selfcare' => false,
+          'check'             => {
+            'Password-Type'     => 'SSHA1-Password',
+            'Auth-Type'         => ''
           },
-          'reply'   => {},
+          'reply'           => {},
         }
 
         def self.get_config
@@ -28,9 +29,10 @@ class OnBoard
         def self.update_config(params)
           FileUtils.mkdir_p CONFDIR # just in case
           config_data = {
-            'enable'  => ( params['enable'] ? true : false )  ,
-            'check'   => params['check']                      ,
-            'reply'   => params['reply']                      ,
+            'enable'          => ( params['enable']           ? true : false )  ,
+            'enable_selfcare' => ( params['enable_selfcare']  ? true : false )  ,
+            'check'           => params['check']                                ,
+            'reply'   => params['reply']                                        ,
           }
           File.open CONFFILE, 'w' do |f|
             f.write config_data.to_yaml
