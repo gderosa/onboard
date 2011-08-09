0c14347978b5a22e3d5272e968ed6eefc1812703
Guido De Rosa <guido.derosa@vemarsas.it>
2011-08-09 11:28:02 +0000

    do not allow POST to signup if disabled

--- modules/radius-admin/controller/pub/services/radius/signup.rb
+++ modules/radius-admin/controller/pub/services/radius/signup.rb
@@ -25,10 +25,20 @@ class OnBoard
     end
 
     post '/pub/services/radius/signup.html' do
+      conf = Service::RADIUS::Signup.get_config
+      unless conf['enable']
+        halt(
+          403, # Forbidden
+          format(
+            :module   => 'radius-admin',
+            :path     => '/pub/services/radius/signup_disabled'
+          )
+        )
+      end
       name  = params['check']['User-Name']
       user  = Service::RADIUS::User.new(name) # blank slate
       msg = handle_errors do
-        h = Service::RADIUS::Signup.get_config.deep_merge(params)
+        h = config.deep_merge(params)
         Service::RADIUS::Check.insert(h)
         user.update_reply_attributes(h) 
         user.update_personal_data(h)
