d1e9ca71e1d2fb2d02d0a9c148665ddccdc78baa
Guido De Rosa <guido.derosa@vemarsas.it>
2011-08-22 17:11:34 +0000

    view list of radius users by terms

--- modules/radius-admin/controller/services/radius/users/by_terms.rb
+++ modules/radius-admin/controller/services/radius/users/by_terms.rb
@@ -12,13 +12,14 @@ class OnBoard
         info = Service::RADIUS::User.by_terms(params)
 
         # merge information about the Terms document itself
-        info[:terms_doc] = Service::RADIUS::Terms::Document.get(
+        info['terms_doc'] = Service::RADIUS::Terms::Document.get(
             params[:terms_id], :content => false ) 
       end
+      not_found unless info['terms_doc'] 
       format(
         :module   => 'radius-admin',
         :path     => '/services/radius/users/by_terms/terms',
-        :title    => "RADIUS Users subscribing ",
+        :title    => "RADIUS Users subscribing Terms and Conditions document ##{info['terms_doc'][:id]}",
         :format   => params[:format],
         :objects  => info,
         :msg      => msg
