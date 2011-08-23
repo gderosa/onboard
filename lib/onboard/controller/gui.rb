5f9f96a7304d01a9bb7ef6d70ce9061cf63bea6d
Guido De Rosa <guido.derosa@vemarsas.it>
2011-08-23 15:25:04 +0000

    public access to icons and beckgrounds

--- lib/onboard/controller/gui.rb
+++ lib/onboard/controller/gui.rb
@@ -5,5 +5,9 @@ class OnBoard
     IconDir = '/icons/gnome/gnome-icon-theme-2.18.0'
     IconSize = '16x16'
 
+    public_access! %r{^/backgrounds/} 
+
+    public_access! %r{^/icons/} 
+
   end
 end
