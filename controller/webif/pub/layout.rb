2d6a05c86f468c8026d816749e36ae9af7f7cce7
Guido De Rosa <guido.derosa@vemarsas.it>
2011-08-24 08:46:03 +0000

    pub/layout: r/w conf

--- controller/webif/pub/layout.rb
+++ controller/webif/pub/layout.rb
@@ -10,9 +10,13 @@ class OnBoard
         :path => '/webif/pub/manage_layout', 
             # not an erubis layout file, hence the name, to avoid confusion ;-)
         :format => params[:format],
-        :objects => nil,
+        :objects => OnBoard::Pub::Layout.read_conf,
         :title  => title
       )
+    end
+
+    get '/webif/pub/logo_preview' do
+      send_file OnBoard::Pub::Layout.logo_file
     end
 
     put '/webif/pub/layout.:format' do 
@@ -21,7 +25,7 @@ class OnBoard
         :path => '/webif/pub/manage_layout', 
             # not an erubis layout file, hence the name, to avoid confusion ;-)
         :format => params[:format],
-        :objects => nil,
+        :objects => OnBoard::Pub::Layout.read_conf,
         :title  => title
       )
     end
--- lib/onboard/pub/layout.rb
+++ lib/onboard/pub/layout.rb
@@ -4,29 +4,38 @@ require 'yaml'
 class OnBoard
   module Pub
     module Layout
-      FILESDIR  = File.join OnBoard::RWDIR, 'var/www/pub'
-      CONFDIR   = File.join OnBoard::CONFDIR, 'webif/pub' 
-      CONFFILE  = File.join CONFDIR, 'layout.yml'
+      FILESDIR        = File.join OnBoard::RWDIR, 'var/www/pub'
+      CONFDIR         = File.join OnBoard::CONFDIR, 'webif/pub' 
+      CONFFILE        = File.join CONFDIR, 'layout.yml'
+      CUSTOMTEXTFILE  = "#{FILESDIR}/custom_text.html"
 
       class << self
 
         def read_conf
+          conf = {}
           if File.exists? CONFFILE
-            return YAML.load File.read CONFFILE
-          else
-            return {}
+            conf = YAML.load File.read CONFFILE
           end
+          if File.exists? CUSTOMTEXTFILE
+            conf['custom_text'] = File.read CUSTOMTEXTFILE
+          end
+          return conf
+        end
+
+        def logo_file
+          conf = read_conf
+          File.join FILESDIR, conf['logo'] if conf['logo'] # else return nil ^_-
         end
 
         def update(params)
-          pp params # DEBUG
-
           FileUtils.mkdir_p FILESDIR
           FileUtils.mkdir_p CONFDIR
 
           conf = read_conf
 
-          if params['logo']
+          if params['delete'] and params['delete']['logo']
+            conf.delete 'logo'
+          elsif params['logo']
             FileUtils.cp(
               params['logo'][:tempfile], 
               File.join(
@@ -38,14 +47,18 @@ class OnBoard
           end
           conf['logo_link'] = params['logo_link']
 
-          File.open(CONFFILE, 'w') do |f|
-            f.write YAML.dump conf
-          end
-          if  params['custom_text'].respond_to? :length and 
+          if params['delete'] and params['delete']['custom_text']
+            conf.delete 'custom_text'
+            FileUtils.rm CUSTOMTEXTFILE if File.exists? CUSTOMTEXTFILE
+          elsif  params['custom_text'].respond_to? :length and 
               params['custom_text'].length >    0           # useful?      
-            File.open("#{FILESDIR}/custom_text.html", 'w') do |f|
+            File.open(CUSTOMTEXTFILE, 'w') do |f|
               f.write params['custom_text']
             end
+          end
+
+          File.open(CONFFILE, 'w') do |f|
+            f.write YAML.dump conf
           end
         end
 
