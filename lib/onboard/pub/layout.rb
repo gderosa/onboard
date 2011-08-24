19c9a555cfe033b2f57a09288c5e5b01be586997
Guido De Rosa <guido.derosa@vemarsas.it>
2011-08-24 07:26:03 +0000

    pub/layout: write files on form submit

--- lib/onboard/pub/layout.rb
+++ lib/onboard/pub/layout.rb
@@ -10,27 +10,43 @@ class OnBoard
 
       class << self
 
+        def read_conf
+          if File.exists? CONFFILE
+            return YAML.load File.read CONFFILE
+          else
+            return {}
+          end
+        end
+
         def update(params)
           pp params # DEBUG
 
           FileUtils.mkdir_p FILESDIR
           FileUtils.mkdir_p CONFDIR
 
-          conf = {}
+          conf = read_conf
 
-          FileUtils.cp(
-            params['logo'][:tempfile], 
-            File.join(
-              FILESDIR, 
-              params['logo'][:filename]
-            )
-          ) 
-          conf['logo'] = params['logo'][:filename]
+          if params['logo']
+            FileUtils.cp(
+              params['logo'][:tempfile], 
+              File.join(
+                FILESDIR, 
+                params['logo'][:filename]
+              )
+            )  
+            conf['logo']      = params['logo'][:filename]
+          end
+          conf['logo_link'] = params['logo_link']
 
           File.open(CONFFILE, 'w') do |f|
             f.write YAML.dump conf
           end
-
+          if  params['custom_text'].respond_to? :length and 
+              params['custom_text'].length >    0           # useful?      
+            File.open("#{FILESDIR}/custom_text.html", 'w') do |f|
+              f.write params['custom_text']
+            end
+          end
         end
 
       end
