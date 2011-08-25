044a3c82115edd0a2f7228cfe8673ab7bc2d2111
Guido De Rosa <guido.derosa@vemarsas.it>
2011-08-25 09:32:09 +0000

    radius: reset db: Terms docs

--- modules/radius-admin/lib/onboard/service/radius/db.rb
+++ modules/radius-admin/lib/onboard/service/radius/db.rb
@@ -20,17 +20,24 @@ class OnBoard
             if params['resetdata']
               if params['resetdata']['default'] =~ /on|yes|true/
                 tables += [
+                  RADIUS.conf['terms_accept']['table'], # has FOREIGN KEYs
                   RADIUS.conf['user']['personal']['table'],
                   RADIUS.conf['user']['check']['table'],
                   RADIUS.conf['user']['reply']['table'],
                   RADIUS.conf['group']['check']['table'],
                   RADIUS.conf['group']['reply']['table'],
-                  RADIUS.conf['group']['usermap']['table']
+                  RADIUS.conf['group']['usermap']['table'],
+                  RADIUS.conf['terms_accept']['table']                  
                 ]
               end
               if params['resetdata']['accounting'] =~ /on|yes|true/
                 tables += [
                   RADIUS.conf['accounting']['table'] 
+                ]
+              end
+              if params['resetdata']['terms'] =~ /on|yes|true/
+                tables += [
+                  RADIUS.conf['terms']['table']
                 ]
               end
             end
