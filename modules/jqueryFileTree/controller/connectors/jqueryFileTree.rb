7c3a8bc33a912929c8c3adb149e35596807972d6
Guido De Rosa <guido.derosa@vemarsas.it>
2012-01-18 12:01:38 +0100

    jqueryFileTree Sinatra connector

--- modules/jqueryFileTree/controller/connectors/jqueryFileTree.rb
+++ modules/jqueryFileTree/controller/connectors/jqueryFileTree.rb
@@ -8,13 +8,55 @@ class OnBoard
     # labs.abeautifulsite.net/archived/jquery-fileTree/jquery.fileTree-1.01.zip
     # An adaptation from CGI to Sinatra.
     get '/connectors/jqueryFileTree/:root_id.html' do
+      root = nil
       out = ''
-      if params['root_id'] == 'files'
-        content_type 'text/plain'
-        out << params.pretty_inspect
+      if params['root_id'] == 'files' # for future extension
+        begin 
+          root = ENV['HOME'] + '/files'
+          dir = params['dir'].to_s
+          path = File.join root, dir
+
+          out << "<ul class=\"jqueryFileTree\" style=\"display:none;\">"
+
+          # chdir() to user requested dir (root + "/" + dir)
+          Dir.chdir(File.expand_path(path).untaint)
+
+          # check that our base path still begins with root path
+          if Dir.pwd[0,root.length] == root
+
+            #loop through all directories
+            Dir.glob("*") do |x|
+              if not File.directory?(x.untaint)
+                next
+              end
+              out << "<li class=\"directory collapsed\"><a href=\"#\" rel=\"#{dir}#{x}/\">#{x}</a></li>"
+            end
+            
+            #loop through all files
+            Dir.glob("*") do |x|
+              if not File.file?(x.untaint) 
+                next
+              end
+              ext = File.extname(x)[1..-1]
+              out << "<li class=\"file ext_#{ext}\"><a href=\"#\" rel=\"#{dir}#{x}\">#{x}</a></li>"
+            end
+            
+            out << "</ul>"
+
+            return out
+
+          else
+            forbidden
+          end
+
+        rescue Errno::ENOENT
+          not_found
+        end
+
       else
-        halt 403, 'Forbidden'
+        not_found
       end
+
     end
 
   end
