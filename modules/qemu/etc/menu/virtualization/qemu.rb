eebb817609989632206ee0fa72bf65732993b664
Guido De Rosa <guido.derosa@vemarsas.it>
2012-01-20 11:00:37 +0100

    qemu: loader end menu entry

---
+++ modules/qemu/etc/menu/virtualization/qemu.rb
@@ -0,0 +1,14 @@
+class OnBoard
+
+  MENU_ROOT.add_path('/virtualization', {
+    :name => 'Virtualization',
+    :n    => 0
+  })
+ 
+  MENU_ROOT.add_path('/virtualization/qemu', {
+    :name => 'QEMU',
+    :href => '/virtualization/qemu',
+    :n    => 0
+  })
+
+end
---
+++ modules/qemu/load.rb
@@ -0,0 +1,14 @@
+class OnBoard
+  module V12n 
+    module QEMU
+      ROOTDIR = File.dirname(__FILE__)
+      # $LOAD_PATH.unshift  ROOTDIR + '/lib'
+      if OnBoard.web?
+        OnBoard.find_n_load ROOTDIR + '/etc/menu'
+        #OnBoard.find_n_load ROOTDIR + '/controller'
+      end
+    end
+  end
+  Virtualization = V12n 
+end
+
