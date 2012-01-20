22f2444e52868df72b6c0ea355bdb43f9a9284c1
Guido De Rosa <guido.derosa@vemarsas.it>
2012-01-20 17:08:27 +0100

    qemu create stub: YAML fixes

--- modules/qemu/lib/onboard/v12n/qemu.rb
+++ modules/qemu/lib/onboard/v12n/qemu.rb
@@ -9,7 +9,6 @@ class OnBoard
 
         class << self
           def http_params2argv(params, opts={}) 
-            pp params
             uuid = opts[:uuid] || UUID.generate
             return "-uuid #{uuid} -m 512 -vnc #{params['vnc']} -drive #{params['drive'][0]['file']},index=0 -daemonize -monitor unix:/var/run/qemu-#{uuid}.sock,server,nowait -pidfile /var/run/qemu-#{uuid}.pid"
           end
@@ -33,7 +32,7 @@ class OnBoard
         def save
           yaml_file = File.join QEMU::CONFDIR, "#@uuid.yml" 
           File.open yaml_file, 'w' do |f|
-            f.write to_yaml
+            f.write YAML.dump export_data
           end
         end
 
