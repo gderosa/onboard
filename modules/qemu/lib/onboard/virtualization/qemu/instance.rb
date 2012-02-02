a5c396759ffa5096c9e43e4c75dd39be89f53536
Guido De Rosa <guido.derosa@vemarsas.it>
2012-02-02 20:00:02 +0100

    qemu: format cmdline

--- modules/qemu/lib/onboard/virtualization/qemu/instance.rb
+++ modules/qemu/lib/onboard/virtualization/qemu/instance.rb
@@ -23,7 +23,40 @@ class OnBoard
         end
 
         def start
-          puts "starting #{uuid}" 
+          opts = @config.cmd['opts'] 
+          exe = Config::Common.get['exe'] 
+          cmdline = ''
+          cmdline << %Q{#{exe} } 
+          %w{
+            -uuid 
+            -name 
+            -m
+            -vnc
+            -pidfile
+          }.each do |o|
+            cmdline << o << ' ' << opts[o] << ' ' if opts[o] =~ /\S/  
+          end
+          cmdline << '-daemonize' << ' ' if opts['-daemonize'] 
+          if opts['-monitor']
+            if opts['-monitor']['unix']
+              unix_args = []
+              unix_args << %Q{unix:"#{opts['-monitor']['unix']}"} 
+              unix_args << 'server' if opts['-monitor']['server']
+              unix_args << 'nowait' if opts['-monitor']['nowait']
+              cmdline << '-monitor ' << unix_args.join(',') << ' '
+            end 
+          end
+          if opts['-drive'].respond_to? :each
+            opts['-drive'].each do |d|
+              drive_args = []
+              drive_args << %Q{file="#{d['file']}"} 
+              drive_args << %Q{media=#{d['media']}} 
+              drive_args << %Q{index=#{d['index']}} if 
+                  d['index'] =~ /\S/
+              cmdline << '-drive ' << drive_args.join(',') << ' '
+            end
+          end
+          puts cmdline
         end
 
       end
