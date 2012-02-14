ad6dba471616dbfe7834f8f985f50c5ccd4739f7
Guido De Rosa <guido.derosa@vemarsas.it>
2012-02-14 11:59:13 +0100

    qemu: netif form: vlan and defaults

--- modules/qemu/lib/onboard/virtualization/qemu/network/nic.rb
+++ modules/qemu/lib/onboard/virtualization/qemu/network/nic.rb
@@ -14,8 +14,9 @@ class OnBoard
               exe = QEMU::Config::Common.get['exe']
               # Assumption: something like
               #   qemu: Supported NIC models: ne2k_pci,i82551,i82557b,i82559er,rtl8139,e1000,pcnet,virtio
-              `#{exe} -net nic,model=? 2>&1` =~ /(\S+)\s?$/
-              $1.split(',') 
+              `#{exe} -net nic,model=? 2>&1` =~ /(\S+)\s?$/ and
+                  $1.split(',')                             or
+                  []  
             end
 
           end
