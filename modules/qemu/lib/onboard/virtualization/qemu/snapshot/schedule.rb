6481f295a9e9db36eb6a2ad32c1e80f556e85ce6
Guido De Rosa <guido.derosa@vemarsas.it>
2012-03-13 12:24:09 +0100

    qemu bin/snapshot: support multiple drives

--- modules/qemu/lib/onboard/virtualization/qemu/snapshot/schedule.rb
+++ modules/qemu/lib/onboard/virtualization/qemu/snapshot/schedule.rb
@@ -9,7 +9,12 @@ class OnBoard
 
             def manage(h)
               enable      = h[:http_params]['snapshot_schedule']['enable']
-              drives      = h[:http_params]['snapshot_schedule']['drives']
+              drive_names = (
+                  h[:http_params]['snapshot_schedule']['drives'].select do |k, v|
+                    v and v != 0 and v != ''
+                  end
+              ).keys
+              comma_separated_drive_names = drive_names.join(',')
               hour        = h[:http_params]['snapshot_schedule']['H']
               minute      = h[:http_params]['snapshot_schedule']['M']
               weekday     = h[:http_params]['snapshot_schedule']['w']
@@ -21,14 +26,14 @@ h[:http_params]['snapshot_schedule']['delete_older_than_days']
               vmid    = h[:http_params]['vmid']            
               cronid  = "qemu_snapshot_#{vmid}"
 
-              if enable
+              if enable 
                 CronEdit::Crontab.Add cronid, {
                   :minute   => minute,
                   :hour     => hour,
                   :day      => dayofmonth,
                   :weekday  => weekday,
                   :command  => 
-"#{QEMU::BINDIR}/snapshot take #{vmid} `date '+scheduled_\\%y\\%m\\%d_\\%H\\%M'`" 
+"#{QEMU::BINDIR}/snapshot take #{vmid} `date '+scheduled_\\%y\\%m\\%d_\\%H\\%M'` #{comma_separated_drive_names}" 
                       # escape cron comment sign '%' 
                       # TODO: patch CronEdit to do this trasparently?
                 } 
