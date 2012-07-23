966820eeaac854ab22ea492152c14dca57e9f804
OnBoard <onboard@mgy-demo.localdomain>
2012-07-23 18:59:16 +0200

    fix for new versions of R18n

--- lib/onboard/controller/locale.rb
+++ lib/onboard/controller/locale.rb
@@ -6,10 +6,14 @@ class OnBoard
     # Extensions must be explicitly registered in modular style apps.
     register ::Sinatra::R18n
 
-    set(
-      :translations, 
-      [File.join(ROOTDIR, 'i18n')] + Dir.glob("#{ROOTDIR}/modules/*/i18n")
-    )
+    R18n.default_places = 
+        [File.join(ROOTDIR, 'i18n')] + 
+        Dir.glob("#{ROOTDIR}/modules/*/i18n")
+    
+    #set(
+    #  :translations, 
+    #  [File.join(ROOTDIR, 'i18n')] + Dir.glob("#{ROOTDIR}/modules/*/i18n")
+    #)
 
   end
 end    
