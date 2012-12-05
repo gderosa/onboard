beed79d17e0ddfc86ab87809a0a439828c9d82a5
Guido De Rosa <guido.derosa@vemarsas.it>
2012-12-05 09:15:18 +0100

    qemu: Sound modules/classes

---
+++ modules/qemu/lib/onboard/virtualization/qemu/sound.rb
@@ -0,0 +1,10 @@
+
+class OnBoard
+  module Virtualization
+    module QEMU
+      module Sound
+        autoload :Hardware, 'onboard/virtualization/qemu/sound/hardware'
+      end
+    end
+  end
+end
