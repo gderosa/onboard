2d2aabe928d7dd98afe3777a072da780f8731a82
Guido De Rosa <guido.derosa@vemarsas.it>
2013-01-03 17:33:32 +0000

    system user/passwd classes

---
+++ lib/onboard/system/user/passwd.rb
@@ -0,0 +1,32 @@
+require 'etc'
+
+class OnBoard
+  module System
+    class User 
+      class Passwd
+
+        autoload :Status, 'onboard/system/user/passwd/status'
+
+        attr_reader :entry, :status
+
+        def initialize(x)
+          @entry = case x
+          when Integer
+            Etc.getpwuid x
+          when String, Symbol
+            Etc.getpwnam x.to_s
+          when Struct::Passwd
+            x
+          end
+          @status = Status.new @entry
+        end
+
+        def locked?
+          # p @status.fields
+          @status.fields[1] == 'L'
+        end
+
+      end
+    end
+  end
+end
---
+++ lib/onboard/system/user/passwd/status.rb
@@ -0,0 +1,17 @@
+class OnBoard
+  module System
+    class User 
+      class Passwd
+        class Status
+          attr_reader :fields
+          def initialize(passwd_entry)
+            @username = passwd_entry.name
+            cmd = "passwd --status #{@username}" 
+            cmd = "sudo #{cmd}" unless Process.uid == passwd_entry.uid
+            @fields = `#{cmd}`.split
+          end
+        end
+      end
+    end
+  end
+end
