bb939e2bba4f284a6fd784047f5a74f05b3bd66d
Guido De Rosa <guido.derosa@vemarsas.it>
2013-01-16 11:05:13 +0000

    put the shutdown menu on bottom

--- etc/menu/system/shutdown.rb
+++ etc/menu/system/shutdown.rb
@@ -1,6 +1,7 @@
 class OnBoard
   MENU_ROOT.add_path('/system/shutdown', {
     :href => '/system/shutdown',
-    :name => 'Shutdown and Reboot'#,
+    :name => 'Shutdown and Reboot',
+    :n    => 99
   })
 end
