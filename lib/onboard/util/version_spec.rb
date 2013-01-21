e28f1f98ad7239c2e2d3f89bcd44aad13fda5e7d
Guido De Rosa <guido.derosa@vemarsas.it>
2013-01-21 19:15:30 +0100

    version_spec.rb more expressive

--- lib/onboard/util/version_spec.rb
+++ lib/onboard/util/version_spec.rb
@@ -6,16 +6,16 @@ include OnBoard::Util
 
 describe Version, '#<=>' do
   it %q{should be Version.new('1.2.3') < '1.3'} do
-    (Version.new('1.2.3') < '1.3').should be_true
+    Version.new('1.2.3').should be < '1.3'
   end
   it %q{should be Version[1, 2, 3] < '1.3'} do
-    (Version[1, 2, 3] < '1.3').should be_true
+    Version[1, 2, 3].should be < '1.3'
   end
   it %q{should be '1.3' > Version.new('1.2.3')} do
-    ('1.3' > Version.new('1.2.3')).should be_true
+    '1.3'.should be > Version.new('1.2.3')
   end
   it %q{should be '1.3' > Version[1, 2, 3]} do
-    ('1.3' > Version[1, 2, 3]).should be_true
+    '1.3'.should be > Version[1, 2, 3]
   end
 end
 
