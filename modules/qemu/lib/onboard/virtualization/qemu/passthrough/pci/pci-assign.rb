6772b0ccebcf3f11f96423d559fe77c9ce872ad6
Guido De Rosa <guido.derosa@vemarsas.it>
2013-05-02 19:03:37 +0200

    qemu: implement PCIAssign.prepare
    (untested)

--- modules/qemu/lib/onboard/virtualization/qemu/passthrough/pci/pci-assign.rb
+++ modules/qemu/lib/onboard/virtualization/qemu/passthrough/pci/pci-assign.rb
@@ -1,3 +1,5 @@
+require 'onboard/system/command'
+
 class OnBoard
   module Virtualization
     module QEMU
@@ -5,10 +7,20 @@ class OnBoard
         module PCI
           module PCIAssign
 
+            System::Command.run 'modprobe pci_stub', :sudo
+
             class << self
   
               def prepare(id)
                 # http://www.linux-kvm.org/page/How_to_assign_devices_with_VT-d_in_KVM
+                if `lspci -n -s #{id}` =~ /(\h\h\h\h):(\h\h\h\h)/ # TODO: move this into LSPCI library
+                  vendor, product = $1, $2
+                  System::Comand.run %Q{echo "#{vendor} #{product}" > /sys/bus/pci/drivers/pci-stub/new_id},  :sudo
+                  System::Comand.run %Q{echo 0000:#{id} > /sys/bus/pci/devices/0000:#{id}/driver/unbind},     :sudo
+                  System::Comand.run %Q{echo 0000:#{id} > /sys/bus/pci/drivers/pci-stub/bind},                :sudo
+                else
+                  raise RuntimeError, "PCI device ``#{id}'' not found"
+                end
               end
 
             end
