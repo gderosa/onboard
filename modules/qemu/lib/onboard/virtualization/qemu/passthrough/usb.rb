b30a8fd9592ef0841bab1a9ebe77d20a62ea4dd9
Guido De Rosa <guido.derosa@vemarsas.it>
2013-05-08 19:12:57 +0200

    qemu: usb passthrough: move device matching logic
    from view to lib

--- modules/qemu/lib/onboard/virtualization/qemu/passthrough/usb.rb
+++ modules/qemu/lib/onboard/virtualization/qemu/passthrough/usb.rb
@@ -6,26 +6,33 @@ class OnBoard
 
           attr_reader :dev
           
+          # lsusbdev is a OnBoard::Hardware::LSUSB
           def initialize(lsusbdev)
-            @usb = lsusbdev # OnBoard::Hardware::LSUSB
+            @dev = lsusbdev
             @all_vm = QEMU.get_all
+          end
+
+          # cnf is an element of vmconfig.opts['-device']
+          # where vmconfig is an OnBoard::Virtualization::QEMU::Config object
+          def match_config?(cnf)
+            return false unless cnf['type'] == 'usb-host' 
+            (
+              cnf['hostbus'] and cnf['hostbus'] == dev.bus_id and
+              (
+                (cnf['hostaddr'] and cnf['hostaddr'] == dev.device_id) or
+                (cnf['hostport'] and cnf['hostport'] == dev.port_id)
+              )
+            ) or (
+              cnf['vendorid']   =~ /(0x)?#{dev.vendor_id}/  and
+              cnf['productid']  =~ /(0x)?#{dev.product_id}/
+            )
           end
 
           def used_by
             @all_vm.each do |vm|
               next unless vm.config.opts['-device'].respond_to? :each
-              vm.config.opts['-device'].each do |cnf|
-                next unless device['type'] == 'usb-host'
-                return vm if (
-                  cnf['hostbus'] and cnf['hostbus'] == dev.bus_id and
-                  (
-                    (cnf['hostaddr'] and cnf['hostaddr'] == dev.device_id) or
-                    (cnf['hostport'] and cnf['hostport'] == dev.port_id)
-                  )
-                ) or (
-                  cnf['vendorid']   =~ /(0x)?#{dev.vendor_id}/  and
-                  cnf['productid']  =~ /(0x)?#{dev.product_id}/
-                )
+              vm.config.opts['-device'].find do |cnf|
+                self.match_config? cnf
               end
             end
             nil
