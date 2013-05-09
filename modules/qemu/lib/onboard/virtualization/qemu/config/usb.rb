464928f6a63d2fa83022a9b80aa81d4dbfdc9b05
Guido De Rosa <guido.derosa@vemarsas.it>
2013-05-10 00:20:11 +0200

    qemu: usb passthrough: guest bus: display driver

--- modules/qemu/lib/onboard/virtualization/qemu/config/usb.rb
+++ modules/qemu/lib/onboard/virtualization/qemu/config/usb.rb
@@ -8,17 +8,17 @@ class OnBoard
             {
               'driver'    => 'piix3-usb-uhci',
               'id'        => 'piix3-uhci',
-              '_comment'  => 'USB 1.1 Controller'
+              '_comment'  => 'USB 1.1'
             },
             {
               'driver'    => 'usb-ehci',
               'id'        => 'ehci',
-              '_comment'  => 'USB 2.0 Controller'
+              '_comment'  => 'USB 2.0'
             },
             {
               'driver'    => 'nec-usb-xhci',
               'id'        => 'xhci',
-              '_comment'  => 'USB 3.0 Controller'
+              '_comment'  => 'USB 3.0'
             },
           ]
         end
