473dcf0f94cf6bbfa7b0137cb4fa4335be47b64d
Guido De Rosa <guido.derosa@vemarsas.it>
2013-05-15 17:45:12 +0200

    core + qemu: ui: DRY highlight color

--- lib/onboard/controller/gui.rb
+++ lib/onboard/controller/gui.rb
@@ -5,6 +5,11 @@ class OnBoard
     IconDir = '/icons/gnome/gnome-icon-theme-2.18.0'
     IconSize = '16x16'
 
+    # TODO? Move all colors etc. here from default.css.erb
+    DEFAULT_THEME = {
+      :highlight       => "#44b"
+    }
+
     public_access! %r{^/backgrounds/} 
 
     public_access! %r{^/icons/} 
