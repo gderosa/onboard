e9bd5a78d9f9db0b7ef18fea6a726c3266c5dce8
Guido De Rosa <guido.derosa@vemarsas.it>
2013-05-20 18:38:53 +0200

    dhcpcd --persistent

--- lib/onboard/network/interface.rb
+++ lib/onboard/network/interface.rb
@@ -469,7 +469,7 @@ class OnBoard
       alias ip_addr_flush flush_ip
 
       def start_dhcp_client
-        success = Command.bgexec "dhcpcd #{@name}", :sudo
+        success = Command.bgexec "dhcpcd -p #{@name}", :sudo
         sleep(0.1) # so the new running process will be detected
         return success
       end
