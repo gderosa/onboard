8a1d331ca514cd22997a8f459c0f91d64f24158b
Guido De Rosa <guido.derosa@vemarsas.it>
2013-09-27 13:24:49 +0200

    listen on any to ease testing (secure this apart)
    A good idea would be listening on 0.0.0.0 only in development...
    
    In the meanwhilem use, well, a firewall and/or a VPN ;)

--- onboard.rb
+++ onboard.rb
@@ -216,7 +216,7 @@ if OnBoard.web?
   #require 'onboard/controller'
   #require 'onboard/controller/helpers'
   if $0 == __FILE__
-    OnBoard::Controller.run!
+    OnBoard::Controller.run! :bind => '0.0.0.0'
   end
 end
 
