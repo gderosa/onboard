0bbf069d52812094f4ccb379bddf1e1820b70926
Guido De Rosa <guido.derosa@vemarsas.it>
2013-11-25 18:44:10 +0100

    qemu/monitor/qmp.rb (lib)

--- modules/qemu/lib/onboard/virtualization/qemu/monitor/qmp.rb
+++ modules/qemu/lib/onboard/virtualization/qemu/monitor/qmp.rb
@@ -1,77 +1,26 @@
 require 'socket'
-require 'timeout'
-
-require 'onboard/extensions/logger'
+require 'json'
 
 class OnBoard
   module Virtualization
     module QEMU
       class Monitor
+        class QMP < Monitor
 
-        # TODO: make this class "abstract", moving most of its logic to
-        # a fresh Monitor::HMP class (human monitor, as opposed to QMP)
-        # See also 
-        # http://git.qemu.org/?p=qemu.git;a=blob_plain;f=docs/qmp/README;hb=HEAD
-
-        autoload :QMP, 'onboard/virtualization/qemu/monitor/qmp'
-
-        def initialize(h)
-          @config = h
-        end
-
-        def unix_path
-          @config['unix']
-        end
-
-        def sendrecv(msg='', opts={}) # UNIXSocket only currently supported
-         
-          out = ''
-          opts = {:timeout => 1}.merge(opts)  
-
-          begin
-            timeout(opts[:timeout]) do
-              UNIXSocket.open(unix_path) do |uds|
-                uds.puts                  # just to get the prompt
-                banner = uds.gets         # unused, just to go ahead, could be empty
-                prompt_long = uds.gets    # "(qemu)" or "(qemu) QEMU 1.5.0 ..."
-	 	prompt_long =~ /^\s*(\S+)/
-		prompt = $1	
-
-                uds.puts msg
-                spurious_line = uds.gets  
-                    # lots of terminal escape sequences, go ahead
-
-                line = ''
-
-                # Last line has no trailing line-terminating char, so we have to
-                # perform some character-level operations.
-
-                while (line.strip != prompt) 
-                    # "(qemu)" or something
-                  c = uds.getc
-                  break unless c
-                  line << c
-                  if c == "\n"
-                    out << line
-                    line = ''
-                  end
-                end
-
-              end # UNIXSocket
-            end # Timeout
-          rescue Timeout::Error, Errno::ECONNRESET, Errno::ECONNREFUSED
-            LOGGER.handled_error $! 
-            out << "[Monitor Error: #{$!}]" unless opts[:on_errors] == :silent 
-            raise if opts[:raise]
+          def execute(command, arguments={})
+            UNIXSocket.open(unix_path) do |uds| # TODO: timeouts
+              greeting = uds.gets
+              uds.puts '{ "execute": "qmp_capabilities" }'
+              capabilities = uds.gets
+              uds.puts JSON.dump( {
+                'execute' => command,
+                'arguments' => arguments
+              } )
+              return uds.gets
+            end
           end
-          if opts[:log] == :verbose  
-            LOGGER.debug "qemu: Monitor socket at #{unix_path}"
-            LOGGER.debug "qemu: message to Monitor: #{msg}"
-            LOGGER.debug "qemu: Monitor result: #{out}" 
-          end
-          return out
-        end
 
+        end
       end
     end
   end
