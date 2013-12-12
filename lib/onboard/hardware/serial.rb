5775fbaf309f47c4cf39d49ce00b76fd97c674cc
Guido De Rosa <guido.derosa@vemarsas.it>
2013-12-12 20:13:39 +0100

    Hardware::Serial::ls

---
+++ lib/onboard/hardware/serial.rb
@@ -0,0 +1,45 @@
+class OnBoard
+  module Hardware
+    module Serial
+
+      class << self
+
+        def ls
+          list            = []
+          base_node_paths = []
+          File.foreach '/proc/tty/drivers' do |line|
+            # http://lwn.net/images/pdf/LDD3/ch18.pdf
+            driver, 
+            base_node_path, 
+            major, 
+            minors, 
+            type = 
+                line.strip.split(/\s+/)
+            if type == 'serial'
+              base_node_paths << base_node_path
+                  # base_node_path is like /dev/ttyS or /dev/ttyUSB
+            end
+          end
+          base_node_paths.each do |base_node_path|
+            node_paths    = []
+            glob_pattern  = "#{base_node_path}*"
+            regexp        = /^#{base_node_path}[0-9]?$/
+            Dir.glob(glob_pattern) do |node_path|
+              node_paths << node_path if node_path =~ regexp
+            end
+            list += node_paths.sort
+          end
+          return list
+        end
+        alias all ls
+
+      end
+
+    end
+  end
+end
+
+if $0 == __FILE__
+  require 'pp'
+  pp OnBoard::Hardware::Serial.all
+end
