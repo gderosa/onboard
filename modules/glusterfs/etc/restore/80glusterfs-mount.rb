ef540cc924fa274739a1c06f2d66ad30b3bfa8e2
Guido De Rosa <guido.derosa@vemarsas.it>
2013-12-18 10:23:31 +0100

    make glusterfs restore idempotent

--- modules/glusterfs/etc/restore/80glusterfs-mount.rb
+++ modules/glusterfs/etc/restore/80glusterfs-mount.rb
@@ -1,4 +1,5 @@
 require 'onboard/system/command'
+require 'onboard/system/fs'
 
 # glusterd might not be started because network saw unconfigured
 # TODO: should be moved under Platform/Debian or such
@@ -7,6 +8,9 @@ OnBoard::System::Command.run "/etc/init.d/glusterfs-server start", :sudo
 Dir.glob "#{OnBoard::FILESDIR}/*network*/gluster/*/*" do |dir|
   if dir =~ %r{([^/]+)/([^/]+)$}
     host, volume = $1, $2
-    OnBoard::System::Command.run "mount -t glusterfs #{host}:/#{volume} '#{dir}'", :sudo
+    OnBoard::System::Command.run( 
+        "mount -t glusterfs #{host}:/#{volume} '#{dir}'", :sudo
+    ) unless
+        OnBoard::System::FS.mountpoint? dir
   end
 end
