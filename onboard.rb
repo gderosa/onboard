8cd97a867a4f3fa04b96b8db2ecdc8866c14e003
OnBoard <onboard@margay-voyage-cf.(none)>
2014-09-06 15:18:37 +0000

    version bump 2014.06.01

--- onboard.rb
+++ onboard.rb
@@ -25,7 +25,7 @@ end
 
 class OnBoard
   LONGNAME          ||= 'OnBoard'
-  VERSION           = '2014.06'
+  VERSION           = '2014.06.01'
 
   PLATFORM          = Platform::Debian # TODO? make it configurable? get rid of Platform?
 
