e0234162e884ad94c7a5efb9db51e2d64299b6f9
Guido De Rosa <dev@vemarsas.it>
2015-07-08 00:12:50 +0100

    Adapt onboard.rb and README to Bundler

--- onboard.rb
+++ onboard.rb
@@ -3,6 +3,11 @@
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__)) + '/lib'
 
 require 'rubygems'
+begin
+  require 'bundler/setup'
+rescue LoadError
+  warn 'Warning: Not leveraging Bundler -- is it installed?'
+end
 require 'find'
 require 'logger'
 require 'etc'
