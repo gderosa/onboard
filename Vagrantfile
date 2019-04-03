f984f2644a38863bb36d955289d15f61e7c10121
Guido De Rosa <guido.derosa@vemarsas.it>
2019-04-03 17:43:40 +0100

    provision: bundle

--- Vagrantfile
+++ Vagrantfile
@@ -60,7 +60,7 @@ Vagrant.configure("2") do |config|
     # Enable provisioning with a shell script. Additional provisioners such as
     # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
     # documentation for more information about their specific syntax and use.
-    mgy.vm.provision "shell", path: "./etc/scripts/platform/debian/setup.sh", args: ["/vagrant"]
+    mgy.vm.provision "shell", path: "./etc/scripts/platform/debian/setup.sh", args: ["/vagrant", "vagrant"]
 
   end
 
--- etc/scripts/platform/debian/setup.sh
+++ etc/scripts/platform/debian/setup.sh
@@ -1,19 +1,38 @@
-# This assumes the user OnBoard/Margay will run as already exists in the system
+#!/bin/bash
+
+# This assumes the user OnBoard/Margay will run-as already exists in the system,
 # and can sudo,
 # and software is copied / placed in the relevant directory with proper
 # ownership/permissions. This script takes
-# control from there. Another script may be implemented for that very initial
-# bootstrap instead.
+# control from there.
+# Another script may be implemented for that very initial
+# bootstrap instead, and will likely not be used by Vagrant but only for
+# deployment on real hardware or "naked" VMs.
 
 PROJECT_ROOT=${1:-'.'}
-
-cd $PROJECT_ROOT
+_USER=${2:-'onboard'}
 
 install_conffiles() {
+	# TODO: /etc/default/margay to set OnBoard user!
 	install -bvC -m 755 doc/sysadm/examples/etc/init.d/onboard 		/etc/init.d/
 	install -bvC -m 644 doc/sysadm/examples/etc/dnsmasq.conf 		/etc/
 	install -bvC -m 644 doc/sysadm/examples/etc/sysctl.conf		    /etc/
 }
+
+bundle_without_all_opts() {
+	# Avoid --without (empty)
+	without_opt=''
+	groups=''
+	for mod in `ls $PROJECT_ROOT/modules` ; do
+		if [ -f $PROJECT_ROOT/modules/$mod/Gemfile ]; then
+			without_opt='--without'
+			groups="$groups $mod"
+		fi
+	done
+	echo "$without_opt $groups" | xargs
+}
+
+cd $PROJECT_ROOT
 
 apt-get update
 # TODO: lighttpd removed: NGINX!
@@ -22,3 +41,9 @@ apt-get -y install ruby ruby-dev ruby-erubis ruby-rack ruby-rack-protection ruby
 gem install --no-ri --no-rdoc bundler
 
 install_conffiles
+
+su - $_USER -c "
+	cd $PROJECT_ROOT
+	# Module names are also Gemfile groups
+	bundle install $(bundle_without_all_opts)
+"
