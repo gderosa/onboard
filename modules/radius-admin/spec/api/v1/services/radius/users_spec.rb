7552f67728430deb47cbc24d368e53103a4e472f
Guido De Rosa <guido.derosa@vemarsas.it>
2019-04-18 16:14:23 +0100

    radius users api: expected different struct

--- modules/radius-admin/spec/api/v1/services/radius/users_spec.rb
+++ modules/radius-admin/spec/api/v1/services/radius/users_spec.rb
@@ -9,104 +9,83 @@ describe 'RADIUS admin' do
       'check': {
         'User-Name': "__user_test",
         'Password-Type': "SSHA1-Password",
-        'User-Password': "p"
+        'User-Password': "pass"
       },
-      'confirm': {  # TODO: make this not required on json?
+      'confirm': {
         'check': {
-          'User-Password': "p"
+          'User-Password': "pass"
         }
       }
     }
   end
 
-  let(:complex_new_user_data) do
+  let(:user_modification_data) do
+    {
+      'check': {
+        'Password-Type': "SSHA1-Password",
+        'User-Password': "newpass",
+        'Auth-Type': "Reject",
+        'check[Login-Time]': "Wk2305-0855,Sa,Su2305-1655"
+      },
+      'confirm': {
+        'check': {
+          'User-Password': "newpass"
+        }
+      }
+    }
+  end
+
+  let(:expected_json_ater_modification) do
+    # Some details may differ e.g. password may be encrypted with a different "salt" etc.
+    <<-END
     {
       "user": {
         "name": "__user_test",
         "check": [
           {
+            "Id": 57,
             "User-Name": "__user_test",
             "Attribute": "User-Name",
             "Operator": ":=",
             "Value": "__user_test"
           },
           {
+            "Id": 59,
             "User-Name": "__user_test",
             "Attribute": "SSHA1-Password",
             "Operator": ":=",
-            "Value": "Yg+Zkt25hotWV4vLYXcEjGZv153BmsHJMilz0+XT15W5J4S78ieoZQ=="
+            "Value": "5E0+LXhlpobHHhmYadzzif1j3CvzrHkNEkb9HSbVB96RIsZl8oUb4w=="
           },
           {
+            "Id": 60,
             "User-Name": "__user_test",
-            "Attribute": "Login-Time",
+            "Attribute": "Auth-Type",
+            "Operator": ":=",
+            "Value": "Reject"
+          },
+          {
+            "Id": 61,
+            "User-Name": "__user_test",
+            "Attribute": "check[Login-Time]",
             "Operator": ":=",
             "Value": "Wk2305-0855,Sa,Su2305-1655"
           }
         ],
         "reply": [
-          {
-            "User-Name": "__user_test",
-            "Attribute": "Reply-Message",
-            "Operator": ":=",
-            "Value": "my reply msg"
-          },
-          {
-            "User-Name": "__user_test",
-            "Attribute": "Session-Timeout",
-            "Operator": ":=",
-            "Value": "7200"
-          },
-          {
-            "User-Name": "__user_test",
-            "Attribute": "Idle-Timeout",
-            "Operator": ":=",
-            "Value": "1800"
-          },
-          {
-            "User-Name": "__user_test",
-            "Attribute": "WISPr-Bandwidth-Max-Down",
-            "Operator": ":=",
-            "Value": "800000"
-          },
-          {
-            "User-Name": "__user_test",
-            "Attribute": "WISPr-Bandwidth-Max-Up",
-            "Operator": ":=",
-            "Value": "400000"
-          }
+
         ],
         "groups": [
 
         ],
         "personal": {
-          "User-Name": "__user_test",
-          "First-Name": "Jonathan",
-          "Last-Name": "Swift",
-          "Email": "johnny@begood.net",
-          "Work-Phone": "+353 1 1234567",
-          "Home-Phone": "+353 1 7654321",
-          "Mobile-Phone": "+353 85 5555555",
-          "Address": "St Patrick's Cathedral",
-          "City": "Dublin",
-          "State": "",
-          "Country": nil,
-          "Postal-Code": "D08 H6X3",
-          "Notes": "A quick note.",
-          "Creation-Date": "2019-04-17 21:20:13 +0000",
-          "Update-Date": "2019-04-17 22:04:35 +0000",
-          "Birth-Date": "1977-11-30",
-          "Birth-City": "Dublin",
-          "Birth-State": "",
-          "ID-Code": "",
           "Attachments": [
 
           ]
         },
-        "accepted_terms": [
-
-        ]
+        "accepted_terms": null
       }
     }
+    END
   end
 
   it "creates a user" do
@@ -120,9 +99,9 @@ describe 'RADIUS admin' do
   end
 
   it "replaces user data" do
-    put_json '/api/v1/services/radius/users/__user_test', complex_new_user_data
+    put_json '/api/v1/services/radius/users/__user_test', user_modification_data
     expect(last_response).to be_ok
-    # puts last_response.body
+    puts last_response.body
   end
 
   it "deletes a user" do
