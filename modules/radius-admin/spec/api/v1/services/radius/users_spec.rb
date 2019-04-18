07d3398d1a8396e15a2a8cf53285092d85fb9f64
Guido De Rosa <guido.derosa@vemarsas.it>
2019-04-18 19:02:14 +0100

    test that rad attr objs *exist* in the 1st place

--- modules/radius-admin/spec/api/v1/services/radius/users_spec.rb
+++ modules/radius-admin/spec/api/v1/services/radius/users_spec.rb
@@ -35,10 +35,14 @@ describe 'RADIUS admin' do
       },
       'reply' => {
         'Reply-Message' => "my reply msg",
-        'Session-Timeout' => 7200,
-        'Idle-Timeout' => 1800,
-        'WISPr-Bandwidth-Max-Down' => 500000,
-        'WISPr-Bandwidth-Max-Up' => 250000
+        # Even RADIUS attribute of numeric type must be strings
+        # (in the database, a single column `Value` hosts all data types...)
+        # We don't want to implement extra per-attr-type conversion logic,
+        # which is left to RADIUS servers and clients.
+        'Session-Timeout' => "7200",
+        'Idle-Timeout' => "1800",
+        'WISPr-Bandwidth-Max-Down' => "500000",
+        'WISPr-Bandwidth-Max-Up' => "250000"
       },
       'confirm' => {
         'check' => {
@@ -48,62 +52,91 @@ describe 'RADIUS admin' do
     }
   end
 
-  let(:expected_json_after_modification) do
-    # Some details may differ e.g. password may be encrypted with a different "salt" etc.
-    <<-END
-    {
-      "user": {
-        "name": "__user_test",
-        "check": [
-          {
-            "Id": 57,
-            "User-Name": "__user_test",
-            "Attribute": "User-Name",
-            "Operator": ":=",
-            "Value": "__user_test"
-          },
-          {
-            "Id": 59,
-            "User-Name": "__user_test",
-            "Attribute": "SSHA1-Password",
-            "Operator": ":=",
-            "Value": "5E0+LXhlpobHHhmYadzzif1j3CvzrHkNEkb9HSbVB96RIsZl8oUb4w=="
-          },
-          {
-            "Id": 60,
-            "User-Name": "__user_test",
-            "Attribute": "Auth-Type",
-            "Operator": ":=",
-            "Value": "Reject"
-          },
-          {
-            "Id": 61,
-            "User-Name": "__user_test",
-            "Attribute": "check[Login-Time]",
-            "Operator": ":=",
-            "Value": "Wk2305-0855,Sa,Su2305-1655"
-          }
-        ],
-        "reply": [
-
-        ],
-        "groups": [
-
-        ],
-        "personal": {
-          "Attachments": [
-
-          ]
-        },
-        "accepted_terms": null
+# Expected GET response body after the modification
+=begin
+{
+  "user": {
+    "name": "__user_test",
+    "check": [
+      {
+        "Id": 197,
+        "User-Name": "__user_test",
+        "Attribute": "User-Name",
+        "Operator": ":=",
+        "Value": "__user_test"
+      },
+      {
+        "Id": 199,
+        "User-Name": "__user_test",
+        "Attribute": "SSHA1-Password",
+        "Operator": ":=",
+        "Value": "SQaMuG003/CnO1uVEUafF87kr0PBW04gV+1Em+G/+bJ3Sj472o58uw=="
+      },
+      {
+        "Id": 200,
+        "User-Name": "__user_test",
+        "Attribute": "Auth-Type",
+        "Operator": ":=",
+        "Value": "Reject"
+      },
+      {
+        "Id": 201,
+        "User-Name": "__user_test",
+        "Attribute": "Login-Time",
+        "Operator": ":=",
+        "Value": "Wk2305-0855,Sa,Su2305-1655"
       }
-    }
-    END
-  end
+    ],
+    "reply": [
+      {
+        "Id": 30,
+        "User-Name": "__user_test",
+        "Attribute": "Reply-Message",
+        "Operator": ":=",
+        "Value": "my reply msg"
+      },
+      {
+        "Id": 31,
+        "User-Name": "__user_test",
+        "Attribute": "Session-Timeout",
+        "Operator": ":=",
+        "Value": "7200"
+      },
+      {
+        "Id": 32,
+        "User-Name": "__user_test",
+        "Attribute": "Idle-Timeout",
+        "Operator": ":=",
+        "Value": "1800"
+      },
+      {
+        "Id": 33,
+        "User-Name": "__user_test",
+        "Attribute": "WISPr-Bandwidth-Max-Down",
+        "Operator": ":=",
+        "Value": "500000"
+      },
+      {
+        "Id": 34,
+        "User-Name": "__user_test",
+        "Attribute": "WISPr-Bandwidth-Max-Up",
+        "Operator": ":=",
+        "Value": "250000"
+      }
+    ],
+    "groups": [
 
-  let(:expected_new_userdata) do
-    parse_json(expected_json_after_modification)
-  end
+    ],
+    "personal": {
+      "Attachments": [
+
+      ]
+    },
+    "accepted_terms": null
+  }
+}
+=end
+
 
   it "creates a user" do
     # cleanup, no matter what
@@ -120,23 +153,24 @@ describe 'RADIUS admin' do
     expect(last_response).to be_ok
 
     actual_check_attributes = parse_json last_response.body, 'user/check'
-    attributes_to_check = ['Auth-Type', 'Login-Time', 'Password-Type']
-    actual_check_attributes.each do |h|
-      attributes_to_check.each do |attribute_name|
-        if h['Attribute'] == attribute_name
-          expect(h['Value']).to eq(user_modification_data['check'][attribute_name])
-        end
-      end
+    attributes_to_check = ['Auth-Type', 'Login-Time']
+    attributes_to_check.each do |attribute_name|
+      selected_attributes = actual_check_attributes.select{|h| h['Attribute'] == attribute_name}
+      expect(selected_attributes.size).to eql(1)
+      expect(selected_attributes[0]['Value']).to eql(user_modification_data['check'][attribute_name])
     end
+
+    # e.g. "SSHA1-Password"
+    password_attributes = actual_check_attributes.select{|h| h['Attribute'] == user_modification_data['check']['Password-Type']}
+    # must exist and be one, we don't check value as it's SHA-1 encrypted + salt
+    expect(password_attributes.size).to eql(1)
 
     actual_reply_attributes = parse_json last_response.body, 'user/reply'
     attributes_to_check = ['Reply-Message', 'Session-Timeout', 'Idle-Timeout', 'WISPr-Bandwidth-Max-Down', 'WISPr-Bandwidth-Max-Up']
-    actual_reply_attributes.each do |h|
-      attributes_to_check.each do |attribute_name|
-        if h['Attribute'] == attribute_name
-          expect(h['Value']).to eq(user_modification_data['reply'][attribute_name])
-        end
-      end
+    attributes_to_check.each do |attribute_name|
+      selected_attributes = actual_reply_attributes.select{|h| h['Attribute'] == attribute_name}
+      expect(selected_attributes.size).to eql(1)
+      expect(selected_attributes[0]['Value']).to eql(user_modification_data['reply'][attribute_name])
     end
   end
 
