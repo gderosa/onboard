ff068acabae0e0a70e57d44e60f1031767467734
Guido De Rosa <guidoderosa@gmail.com>
2019-10-11 15:26:29 +0100

    add usbutils

--- etc/scripts/platform/debian/setup.sh
+++ etc/scripts/platform/debian/setup.sh
@@ -75,7 +75,7 @@ cd $PROJECT_ROOT
 
 apt-get update
 apt-get -y upgrade
-apt-get -y install ruby ruby-dev ruby-erubis ruby-rack ruby-rack-protection ruby-locale ruby-facets sudo iproute2 iptables bridge-utils pciutils dhcpcd5 dnsmasq resolvconf locales ifrename build-essential ca-certificates ntp psmisc
+apt-get -y install ruby ruby-dev ruby-erubis ruby-rack ruby-rack-protection ruby-locale ruby-facets sudo iproute2 iptables bridge-utils pciutils usbutils dhcpcd5 dnsmasq resolvconf locales ifrename build-essential ca-certificates ntp psmisc
 # Optional, but useful tools when ssh'ing
 apt-get -y install vim-nox mc
 
