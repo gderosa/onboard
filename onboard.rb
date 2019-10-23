c84f2da17cf9751936f41ce52cc1910e64e9662f
Guido De Rosa <guidoderosa@gmail.com>
2019-10-23 16:47:50 +0100

    version bump 2019.10.01

--- onboard.rb
+++ onboard.rb
@@ -30,7 +30,7 @@ end
 
 class OnBoard
   LONGNAME          ||= 'OnBoard'
-  VERSION           = '2019.10'
+  VERSION           = '2019.10.01'
 
   PLATFORM          = Platform::Debian # TODO? make it configurable? get rid of Platform?
 
