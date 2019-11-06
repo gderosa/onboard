34ef50a59536c88229ceff9f4c0e7ac9d624b32a
Guido De Rosa <guidoderosa@gmail.com>
2019-11-06 04:44:48 +0000

    fix view/download logs

--- controller/system/logs.rb
+++ controller/system/logs.rb
@@ -20,15 +20,16 @@ class OnBoard::Controller
   end
 
   get %r{/system/logs/(.*)\.raw} do
-    logid = params['captures'].first  # could be a full path or an identifying basname
+    logid = params['captures'].first
+      # Could be a full path or an identifying basname,
+      # however, leading / is stripped from full paths url-encoded in the (.*)
     potential_absolute_path = File.join '/', logid
     if potential_absolute_path.start_with? OnBoard::LOGDIR
       path = potential_absolute_path
     else
       # Require "registration" for logs outside ~/.onboard/var/log
-      pp OnBoard::System::Log.getAll
       hash = OnBoard::System::Log.getAll.detect do |h|
-        h['id'] == logid or h['path'] == logid
+        h['id'] == logid or h['path'] == (File.join '/', logid)
       end
       not_found if not hash
       path = hash['path']
@@ -43,15 +44,17 @@ class OnBoard::Controller
   end
 
   # by id
-  get "/system/logs/:logid.:format" do
-    hash = OnBoard::System::Log.getAll.detect {|h| h['id'] == params['logid']}
+  get %r{/system/logs/(.*)\.([a-z]+)} do
+    logid, fmt = params['captures']
+    # Leading / is stripped from logid/path
+    hash = OnBoard::System::Log.getAll.detect {|h| h['id'] == logid or h['path'] == (File.join '/', logid)}
     pass if not hash
     log = OnBoard::System::Log.new(hash)
     format(
       :path     => 'system/logs',
-      :format   => params[:format],
+      :format   => fmt,
       :objects  => log,
-      :title    => "Log: #{params['logid']}"
+      :title    => "Log: #{logid}"
     )
   end
 
