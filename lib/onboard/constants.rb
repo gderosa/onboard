eed2bdd0d53cee628b7ea7bbdf0a8e162dcab576
Guido De Rosa <guidoderosa@gmail.com>
2019-11-13 04:54:44 +0100

    fix LOGGER

--- lib/onboard/constants.rb
+++ lib/onboard/constants.rb
@@ -1,16 +1,28 @@
+require 'logger'
+
 # TODO: move all constants here and DRY with root onboard.rb
 # At the moment this is mostly useful from standalone scripts like openvpn --up etc.
 class OnBoard
+  LONGNAME          ||= 'OnBoard'
+  VERSION           = '2019.13.1'
   FILESDIR            ||= File.join ENV['HOME'], 'files' # mass storage...
-
-  ROOTDIR = File.join(File.dirname(__FILE__), '../..') unless defined? ROOTDIR
+  ROOTDIR = File.join(File.dirname(__FILE__), '../..')
   RWDIR = (
     ENV['ONBOARD_RWDIR'] or
     ENV['ONBOARD_DATADIR'] or
     File.join(ENV['HOME'], '.onboard')
-  ) unless defined? RWDIR
-  DATADIR = RWDIR unless defined? DATADIR
-  CONFDIR = File.join RWDIR, '/etc/config' unless defined? CONFDIR
+  )
+  DATADIR = RWDIR
+  CONFDIR = File.join RWDIR, '/etc/config'
+  # sometimes files are uploaded elsewhere, as best suitable
+  DEFAULT_UPLOAD_DIR  = File.join RWDIR, '/var/uploads'
+  LOGDIR = File.join RWDIR, '/var/log'
+  LOGFILE_BASENAME    = 'onboard.log'
+  LOGFILE_PATH        = File.join LOGDIR, LOGFILE_BASENAME
+  VARRUN              ||= '/var/run/onboard'
+  VARLIB              ||= File.join RWDIR, 'var/lib'
+
+  LOGGER = Logger.new(LOGFILE_PATH)
 end
 
 
