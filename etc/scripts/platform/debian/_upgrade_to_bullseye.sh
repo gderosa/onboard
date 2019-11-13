226bde842366cfe3d398559a57db42f60938aaf6
Guido De Rosa <guidoderosa@gmail.com>
2019-11-13 01:21:42 +0100

    dist-upgrade any release (e.g. if net failed previously)

--- etc/scripts/platform/debian/_upgrade_to_bullseye.sh
+++ etc/scripts/platform/debian/_upgrade_to_bullseye.sh
@@ -4,15 +4,11 @@ SOURCES_LIST='/etc/apt/sources.list'
 do_upgrade_to_bullseye() {
     cp $SOURCES_LIST $SOURCES_LIST.buster
     sed -e 's/buster/bullseye/g' $SOURCES_LIST.buster | sed -e 's/^\s*deb-src/#deb-src/' > $SOURCES_LIST
-    apt-get update
-    apt-get -y dist-upgrade
-    apt-get -y upgrade
-    apt-get -y autoremove
 }
 
 if (egrep '^[^#]+buster' $SOURCES_LIST > /dev/null); then
-    echo "Upgrade to Debian 11 (\"bullseye\")? Answer:"
-    echo "  'Y' or 'y' to upgrade;"
+    echo "Switch to Debian 11 (\"bullseye\")? Answer:"
+    echo "  'Y' or 'y' to switch (dist-upgrade will follow);"
     echo "  'N' or 'n' to continue with Debian 10 (buster) and use QEMU 3 instead of 4 (not recommended);"
     echo "  'Q', 'q' or anything else to exit."
     read yn
@@ -26,3 +22,8 @@ if (egrep '^[^#]+buster' $SOURCES_LIST > /dev/null); then
             exit
     esac
 fi
+
+apt-get update
+apt-get -y dist-upgrade
+apt-get -y upgrade
+apt-get -y autoremove
