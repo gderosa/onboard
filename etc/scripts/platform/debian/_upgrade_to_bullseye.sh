fc3c3b1c68dc52af22fe6f61e8cd1d9902c4b8bc
Guido De Rosa <guidoderosa@gmail.com>
2019-11-13 01:04:13 +0100

    Interactive script to upgrade to bullseye

--- etc/scripts/platform/debian/_upgrade_to_bullseye.sh
+++ etc/scripts/platform/debian/_upgrade_to_bullseye.sh
@@ -1,10 +1,28 @@
 SOURCES_LIST='/etc/apt/sources.list'
-if (egrep '^[^#]+buster' $SOURCES_LIST); then
+
+
+do_upgrade_to_bullseye() {
     cp $SOURCES_LIST $SOURCES_LIST.buster
     sed -e 's/buster/bullseye/g' $SOURCES_LIST.buster | sed -e 's/^\s*deb-src/#deb-src/' > $SOURCES_LIST
     apt-get update
     apt-get -y dist-upgrade
     apt-get -y upgrade
-fi
+    apt-get -y autoremove
+}
 
-apt-get -y autoremove
+if (egrep '^[^#]+buster' $SOURCES_LIST > /dev/null); then
+    echo "Upgrade to Debian 11 (\"bullseye\")? Answer:"
+    echo "  'Y' or 'y' to upgrade;"
+    echo "  'N' or 'n' to continue with Debian 10 (buster) and use QEMU 3 instead of 4 (not recommended);"
+    echo "  'Q', 'q' or anything else to exit."
+    read yn
+    case $yn in
+        [Yy] )
+            do_upgrade_to_bullseye
+            ;;
+        [Nn] )
+            ;;
+        * )
+            exit
+    esac
+fi
