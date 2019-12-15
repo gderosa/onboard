2f7c16053a27f7cee7af07bc0f6da45a19fe3f13
Guido De Rosa <guidoderosa@gmail.com>
2019-12-15 11:58:52 +0000

    remove redundancies with margay/setup

--- etc/scripts/platform/debian/setup.sh
+++ etc/scripts/platform/debian/setup.sh
@@ -86,9 +86,7 @@ cd $PROJECT_ROOT
 apt-get update
 apt-get -y upgrade
 
-apt-get -y install ruby ruby-dev ruby-erubis ruby-rack ruby-rack-protection ruby-locale ruby-facets sudo iproute2 iptables bridge-utils pciutils usbutils usb-modeswitch dhcpcd5 openssh-server dnsmasq resolvconf locales ifrename build-essential ca-certificates ntp psmisc
-# Optional, but useful tools when ssh'ing
-apt-get -y install vim-nox mc
+apt-get -y install ruby ruby-dev ruby-erubis ruby-rack ruby-rack-protection ruby-locale ruby-facets sudo iproute2 iptables bridge-utils pciutils usbutils usb-modeswitch dhcpcd5 dnsmasq resolvconf locales ifrename build-essential ca-certificates ntp psmisc
 
 install_conffiles
 
@@ -180,7 +178,3 @@ then
 fi
 
 . $SCRIPTDIR/_restore_dns.sh
-
-# Apparently necessary on Raspbian
-systemctl start ssh
-systemctl enable ssh
