7fff703cbb1dd9d03a42707ae6767a85d405d5eb
Guido De Rosa <guidoderosa@gmail.com>
2021-03-27 00:36:26 +0000

    version

--- lib/onboard/constants.rb
+++ lib/onboard/constants.rb
@@ -1,6 +1,6 @@
 class OnBoard
   LONGNAME          ||= 'OnBoard'
-  VERSION           = '2021.03'
+  VERSION           = '2021.04'
   FILESDIR            ||= File.join ENV['HOME'], 'files' # mass storage...
   ROOTDIR = File.join(File.dirname(__FILE__), '../..')
   RWDIR = (
