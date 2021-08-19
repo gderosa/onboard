fa40c00d8baa4ca034cd50b5f831415f8e95c23c
Guido De Rosa <guido.derosa@vemarsas.it>
2021-08-19 23:48:52 +0200

    bullseye

--- etc/scripts/platform/debian/setup.sh
+++ etc/scripts/platform/debian/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set +e
+
 # This assumes that the user
 # OnBoard/Margay will run-as
 # already exists in the system and can sudo,
@@ -85,7 +87,7 @@ cd $PROJECT_ROOT
 apt-get update
 apt-get -y upgrade
 
-apt-get -y install ruby ruby-dev ruby-erubis ruby-rack ruby-rack-protection ruby-locale ruby-facets sudo iproute2 iptables bridge-utils pciutils usbutils usb-modeswitch dhcpcd5 dnsmasq resolvconf locales ifrename build-essential ca-certificates ntp psmisc
+apt-get -y install ruby ruby-dev ruby-erubis ruby-rack ruby-rack-protection ruby-locale sudo iproute2 iptables bridge-utils pciutils usbutils usb-modeswitch dhcpcd5 dnsmasq resolvconf locales ifrename build-essential ca-certificates libssl-dev ntp psmisc
 
 install_conffiles
 
